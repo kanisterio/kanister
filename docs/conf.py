89519c786198f17105a3a1c9f306049d9e9707a8
Thomas Manville <tom@kasten.io>
2018-02-21 17:59:16 -0800

    Remove compat settings in sphinx docs (#2391)

--- docs/conf.py
+++ docs/conf.py
@@ -21,11 +21,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-# Workaround https://github.com/spinus/sphinxcontrib-images/issues/41
-from docutils.parsers.rst.directives.admonitions import BaseAdmonition
-from sphinx.util import compat
-compat.make_admonition = BaseAdmonition
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
