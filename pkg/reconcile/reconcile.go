6d01f4ff52e71667dbb9ebcf5cbe05b05f05d781
Thomas Manville <tom@kasten.io>
2018-03-23 11:00:38 -0700

    library to reconcile concurrent Actionset updates (#2569)
    * Add the poll package to kanister.
    
    * library to reconcile concurrent Actionset updates
    
    * Address review feedback

---
+++ pkg/reconcile/reconcile.go
@@ -0,0 +1,38 @@
+package reconcile
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pkg/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	crv1alpha1 "github.com/kanisterio/kanister/pkg/apis/cr/v1alpha1"
+	crclientv1alpha1 "github.com/kanisterio/kanister/pkg/client/clientset/versioned/typed/cr/v1alpha1"
+	"github.com/kanisterio/kanister/pkg/poll"
+)
+
+// ActionSet attempts to reconcile the modifications made by `f` with the
+// ActionSet stored in the API server.
+func ActionSet(ctx context.Context, cli crclientv1alpha1.CrV1alpha1Interface, ns, name string, f func(*crv1alpha1.ActionSet) error) error {
+	return poll.Wait(ctx, func(ctx context.Context) (bool, error) {
+		as, err := cli.ActionSets(ns).Get(name, v1.GetOptions{})
+		if err != nil {
+			return false, errors.WithStack(err)
+		}
+		if err = f(as); err != nil {
+			return false, err
+		}
+		_, err = cli.ActionSets(as.GetNamespace()).Update(as)
+		// If we get a version conflict, we backoff and try again.
+		if apierrors.IsConflict(err) {
+			return false, nil
+		}
+		if err != nil {
+			msg := fmt.Sprintf("Failed to update ActionSet %s", name)
+			return false, errors.Wrap(err, msg)
+		}
+		return true, nil
+	})
+}
