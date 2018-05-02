a448e662da8b75ea152f1bbfd179c7869c9d2573
Pavan Navarathna Devaraj <pavan@kasten.io>
2018-05-02 16:15:22 -0700

    Change Groupname for fake client (#2896)

--- pkg/client/clientset/versioned/typed/cr/v1alpha1/fake/fake_blueprint.go
+++ pkg/client/clientset/versioned/typed/cr/v1alpha1/fake/fake_blueprint.go
@@ -18,7 +18,7 @@ type FakeBlueprints struct {
 
 var blueprintsResource = schema.GroupVersionResource{Group: "cr", Version: "v1alpha1", Resource: "blueprints"}
 
-var blueprintsKind = schema.GroupVersionKind{Group: "cr", Version: "v1alpha1", Kind: "Blueprint"}
+var blueprintsKind = schema.GroupVersionKind{Group: "cr.kanister.io", Version: "v1alpha1", Kind: "Blueprint"}
 
 // Get takes name of the blueprint, and returns the corresponding blueprint object, and an error if there is any.
 func (c *FakeBlueprints) Get(name string, options v1.GetOptions) (result *v1alpha1.Blueprint, err error) {
