e55a850675f028276ce8b6b74d54c833ee64e191
Gautham Sunjay <gautham@kasten.io>
2018-07-09 19:04:37 -0700

    Add CLI option to set Options - kanctl perform (#3370)
    * Add CLI option to set Options - kanctl perform
    
    * Addressed comments in review

--- pkg/kanctl/perform_from.go
+++ pkg/kanctl/perform_from.go
@@ -3,6 +3,7 @@ package kanctl
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -13,7 +14,10 @@ import (
 	"github.com/kanisterio/kanister/pkg/kube"
 )
 
-const sourceFlagName = "from"
+const (
+	sourceFlagName  = "from"
+	optionsFlagName = "options"
+)
 
 func newPerformFromCommand() *cobra.Command {
 	cmd := &cobra.Command{
@@ -25,13 +29,13 @@ func newPerformFromCommand() *cobra.Command {
 		},
 	}
 	cmd.PersistentFlags().StringP(sourceFlagName, "f", "", "specify name of the action set(required)")
+	cmd.Flags().StringSliceP(optionsFlagName, "o", []string{}, "specify options for the action set, comma separated key=value pairs (eg: --options key1=value1,key2=value2)")
 	cmd.MarkFlagRequired(sourceFlagName)
 	return cmd
 
 }
 
 func runPerformFrom(cmd *cobra.Command, args []string) error {
-
 	parentName := cmd.Flag(sourceFlagName).Value.String()
 	if len(args) != 1 {
 		return newArgsLengthError("expected 1 argument. got %#v", args)
@@ -41,11 +45,16 @@ func runPerformFrom(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
+	options, err := parseOptions(cmd)
+	if err != nil {
+		return err
+	}
+
 	ctx := context.Background()
-	return performFrom(ctx, cmd, ns, actionName, parentName)
+	return performFrom(ctx, cmd, ns, actionName, parentName, options)
 }
 
-func performFrom(ctx context.Context, cmd *cobra.Command, namespace string, actionName string, parentName string) error {
+func performFrom(ctx context.Context, cmd *cobra.Command, namespace string, actionName string, parentName string, options map[string]string) error {
 	config, err := kube.LoadConfig()
 	if err != nil {
 		return err
@@ -58,14 +67,14 @@ func performFrom(ctx context.Context, cmd *cobra.Command, namespace string, acti
 	if err != nil {
 		return err
 	}
-	as, err := childActionSet(actionName, pas)
+	as, err := childActionSet(actionName, pas, options)
 	if err != nil {
 		return err
 	}
 	return createActionSet(ctx, cmd, cli, namespace, as)
 }
 
-func childActionSet(childActionName string, parent *crv1alpha1.ActionSet) (*crv1alpha1.ActionSet, error) {
+func childActionSet(childActionName string, parent *crv1alpha1.ActionSet, options map[string]string) (*crv1alpha1.ActionSet, error) {
 	if parent.Status == nil || parent.Status.State != crv1alpha1.StateComplete {
 		return nil, errors.Errorf("Request parent ActionSet %s has not been executed", parent.GetName())
 	}
@@ -79,7 +88,7 @@ func childActionSet(childActionName string, parent *crv1alpha1.ActionSet) (*crv1
 			Secrets:    parent.Spec.Actions[aidx].Secrets,
 			ConfigMaps: parent.Spec.Actions[aidx].ConfigMaps,
 			Profile:    parent.Spec.Actions[aidx].Profile,
-			Options:    parent.Spec.Actions[aidx].Options,
+			Options:    mergeOptions(options, parent.Spec.Actions[aidx].Options),
 		})
 	}
 	name := fmt.Sprintf("%s-%s-", childActionName, parent.GetName())
@@ -99,4 +108,31 @@ func createActionSet(ctx context.Context, cmd *cobra.Command, cli crclientv1alph
 		cmd.Println(fmt.Sprintf("actionset %s created", as.Name))
 	}
 	return err
+}
+
+func parseOptions(cmd *cobra.Command) (map[string]string, error) {
+	optionsFromCmd, _ := cmd.Flags().GetStringSlice(optionsFlagName)
+	options := make(map[string]string)
+
+	for _, kv := range optionsFromCmd {
+		// Cobra takes care of trimming spaces
+		kvPair := strings.Split(kv, "=")
+		if len(kvPair) != 2 {
+			return nil, errors.Errorf("Expected options as key=value pairs. Got %s", kv)
+		}
+		options[kvPair[0]] = kvPair[1]
+	}
+	return options, nil
+}
+
+func mergeOptions(src map[string]string, dst map[string]string) map[string]string {
+	final := make(map[string]string, len(src)+len(dst))
+	for k, v := range dst {
+		final[k] = v
+	}
+	// Override default options and set additional ones
+	for k, v := range src {
+		final[k] = v
+	}
+	return final
 }
