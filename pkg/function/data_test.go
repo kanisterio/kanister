6b71430e0c8407ba35768f392f69f96df08d45ea
Vaibhav Kamra <vkamra@kasten.io>
2019-03-09 18:29:11 -0800

    Kanister docs update to version 0.17.0 (#5171)

--- pkg/function/data_test.go
+++ pkg/function/data_test.go
@@ -80,7 +80,7 @@ func newRestoreDataBlueprint(pvc string) *crv1alpha1.Blueprint {
 						Func: "RestoreData",
 						Args: map[string]interface{}{
 							RestoreDataNamespaceArg:            "{{ .StatefulSet.Namespace }}",
-							RestoreDataImageArg:                "kanisterio/kanister-tools:0.16.0",
+							RestoreDataImageArg:                "kanisterio/kanister-tools:0.17.0",
 							RestoreDataBackupArtifactPrefixArg: "{{ .Profile.Location.Bucket }}/{{ .Profile.Location.Prefix }}",
 							RestoreDataRestorePathArg:          "/mnt/data",
 							RestoreDataBackupIdentifierArg:     "{{ .Time }}",
@@ -229,7 +229,7 @@ func newCopyDataTestBlueprint() crv1alpha1.Blueprint {
 						Func: "RestoreData",
 						Args: map[string]interface{}{
 							RestoreDataNamespaceArg:            "{{ .PVC.Namespace }}",
-							RestoreDataImageArg:                "kanisterio/kanister-tools:0.16.0",
+							RestoreDataImageArg:                "kanisterio/kanister-tools:0.17.0",
 							RestoreDataBackupArtifactPrefixArg: fmt.Sprintf("{{ .Options.%s }}", CopyVolumeDataOutputBackupArtifactLocation),
 							RestoreDataBackupIdentifierArg:     fmt.Sprintf("{{ .Options.%s }}", CopyVolumeDataOutputBackupID),
 							RestoreDataVolsArg: map[string]string{
