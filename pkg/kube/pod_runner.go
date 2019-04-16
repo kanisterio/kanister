0811dab3def2050eecf46e59a4511833c4b61fee
DeepikaDixit <deepika@kasten.io>
2019-04-16 15:59:47 -0700

    Add pod_runner helper (#5418)
    * Add pod_exec helper
    
    * Rename to pod_runner
    
    * Add waitForCompletion
    
    * Add context to closure
    
    * Change return type to map[string]interface{}
    
    * Wait for PodReady instead of PodComplete
    
    * Add pod_runner_test
    
    * Address review comments

---
+++ pkg/kube/pod_runner.go
@@ -0,0 +1,34 @@
+package kube
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+type PodRunner struct {
+	cli        kubernetes.Interface
+	podOptions *PodOptions
+}
+
+func (p *PodRunner) Run(ctx context.Context, fn func(context.Context, *v1.Pod) (map[string]interface{}, error)) (map[string]interface{}, error) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	pod, err := CreatePod(ctx, p.cli, p.podOptions)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Failed to create pod")
+	}
+	go func() {
+		select {
+		case <-ctx.Done():
+			defer DeletePod(context.Background(), p.cli, pod)
+		}
+	}()
+	// Wait for pod to reach running state
+	if err := WaitForPodReady(ctx, p.cli, pod.Namespace, pod.Name); err != nil {
+		return nil, errors.Wrapf(err, "Failed while waiting for Pod %s to be ready", pod.Name)
+	}
+	return fn(ctx, pod)
+}
---
+++ pkg/kube/pod_runner_test.go
@@ -0,0 +1,57 @@
+package kube
+
+import (
+	"context"
+
+	. "gopkg.in/check.v1"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/testing"
+)
+
+type PodRunnerTestSuite struct{}
+
+var _ = Suite(&PodRunnerTestSuite{})
+
+func (s *PodRunnerTestSuite) TestPodRunnerContextCanceled(c *C) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cli := fake.NewSimpleClientset()
+	cli.PrependReactor("create", "pods", func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+		return false, nil, nil
+	})
+	cli.PrependReactor("get", "pods", func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+		p := &v1.Pod{
+			Status: v1.PodStatus{
+				Phase: v1.PodRunning,
+			},
+		}
+		return true, p, nil
+	})
+	deleted := make(chan struct{})
+	cli.PrependReactor("delete", "pods", func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+		c.Log("Pod Deleted due to Context Cancelled")
+		close(deleted)
+		return true, nil, nil
+	})
+	pr := PodRunner{
+		cli:        cli,
+		podOptions: &PodOptions{},
+	}
+	returned := make(chan struct{})
+	go func() {
+		_, err := pr.Run(ctx, makePodRunnerTestFunc(deleted))
+		c.Assert(err, IsNil)
+		close(returned)
+	}()
+	cancel()
+	<-deleted
+	<-returned
+}
+
+func makePodRunnerTestFunc(deleted chan struct{}) func(ctx context.Context, pod *v1.Pod) (map[string]interface{}, error) {
+	return func(ctx context.Context, pod *v1.Pod) (map[string]interface{}, error) {
+		<-deleted
+		return nil, nil
+	}
+}
