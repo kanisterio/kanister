fba04ea236373d92df8aa27cf27db0e38f0edf80
DeepikaDixit <deepika@kasten.io>
2019-04-18 13:07:49 -0700

    Modify kube_task to use PodRunner (#5429)
    * Modify kube_task to use PodRunner
    
    * Fix imports

--- pkg/function/kube_task.go
+++ pkg/function/kube_task.go
@@ -2,15 +2,15 @@ package function
 
 import (
 	"context"
-	"math/rand"
-	"strconv"
+
+	"github.com/pkg/errors"
+	"k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
 
 	kanister "github.com/kanisterio/kanister/pkg"
 	"github.com/kanisterio/kanister/pkg/format"
 	"github.com/kanisterio/kanister/pkg/kube"
 	"github.com/kanisterio/kanister/pkg/param"
-	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
 )
 
 const (
@@ -32,58 +32,46 @@ func (*kubeTaskFunc) Name() string {
 	return "KubeTask"
 }
 
-func generateJobName(jobPrefix string) string {
-	jobNameSuffix := strconv.FormatUint(rand.Uint64(), 10)
-	return jobPrefix + jobNameSuffix
-}
-
-func kubeTask(ctx context.Context, namespace, image string, command []string) (map[string]interface{}, error) {
+func kubeTask(ctx context.Context, cli kubernetes.Interface, namespace, image string, command []string) (map[string]interface{}, error) {
 	var serviceAccount string
 	var err error
-	clientset, err := kube.NewClient()
-	if err != nil {
-		return nil, errors.Wrapf(err, "Failed to create Kubernetes client")
-	}
 	if namespace == "" {
 		namespace, err = kube.GetControllerNamespace()
 		if err != nil {
 			return nil, errors.Wrapf(err, "Failed to get controller namespace")
 		}
-		serviceAccount, err = kube.GetControllerServiceAccount(clientset)
+		serviceAccount, err = kube.GetControllerServiceAccount(cli)
 		if err != nil {
 			return nil, errors.Wrap(err, "Failed to get Controller Service Account")
 		}
 	}
-	// Create a pod to run the command
-	pod, err := kube.CreatePod(ctx, clientset, &kube.PodOptions{
+	options := &kube.PodOptions{
 		Namespace:          namespace,
 		GenerateName:       jobPrefix,
 		Image:              image,
 		Command:            command,
 		ServiceAccountName: serviceAccount,
-	})
-	if err != nil {
-		return nil, errors.Wrapf(err, "Failed to create pod for KubeTask")
 	}
-	defer func() {
-		if err := kube.DeletePod(context.Background(), clientset, pod); err != nil {
-			log.Error("Failed to delete pod ", err.Error())
+	pr := kube.NewPodRunner(cli, options)
+	podFunc := kubeTaskPodFunc(cli)
+	return pr.Run(ctx, podFunc)
+}
+
+func kubeTaskPodFunc(cli kubernetes.Interface) func(ctx context.Context, pod *v1.Pod) (map[string]interface{}, error) {
+	return func(ctx context.Context, pod *v1.Pod) (map[string]interface{}, error) {
+		// Wait for pod completion
+		if err := kube.WaitForPodCompletion(ctx, cli, pod.Namespace, pod.Name); err != nil {
+			return nil, errors.Wrapf(err, "Failed while waiting for Pod %s to complete", pod.Name)
 		}
-	}()
-
-	// Wait for pod completion
-	if err := kube.WaitForPodCompletion(ctx, clientset, pod.Namespace, pod.Name); err != nil {
-		return nil, errors.Wrapf(err, "Failed while waiting for Pod %s to complete", pod.Name)
+		// Fetch logs from the pod
+		logs, err := kube.GetPodLogs(ctx, cli, pod.Namespace, pod.Name)
+		if err != nil {
+			return nil, errors.Wrapf(err, "Failed to fetch logs from the pod")
+		}
+		format.Log(pod.Name, pod.Spec.Containers[0].Name, logs)
+		out, err := parseLogAndCreateOutput(logs)
+		return out, errors.Wrap(err, "Failed to parse phase output")
 	}
-	// Fetch logs from the pod
-	logs, err := kube.GetPodLogs(ctx, clientset, pod.Namespace, pod.Name)
-	if err != nil {
-		return nil, errors.Wrapf(err, "Failed to fetch logs from the pod")
-	}
-	format.Log(pod.Name, pod.Spec.Containers[0].Name, logs)
-
-	out, err := parseLogAndCreateOutput(logs)
-	return out, errors.Wrap(err, "Failed to parse phase output")
 }
 
 func (ktf *kubeTaskFunc) Exec(ctx context.Context, tp param.TemplateParams, args map[string]interface{}) (map[string]interface{}, error) {
@@ -99,7 +87,11 @@ func (ktf *kubeTaskFunc) Exec(ctx context.Context, tp param.TemplateParams, args
 	if err = OptArg(args, KubeTaskNamespaceArg, &namespace, ""); err != nil {
 		return nil, err
 	}
-	return kubeTask(ctx, namespace, image, command)
+	cli, err := kube.NewClient()
+	if err != nil {
+		return nil, errors.Wrapf(err, "Failed to create Kubernetes client")
+	}
+	return kubeTask(ctx, cli, namespace, image, command)
 }
 
 func (*kubeTaskFunc) RequiredArgs() []string {
