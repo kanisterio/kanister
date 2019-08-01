7df5604646503c65d315b7765516b5d5d5b963e9
Omkar Joshi <joshiomkarj@gmail.com>
2019-08-01 19:37:39 +0530

    Added the code generated for types and controller by operator sdk

---
+++ operator/kanister-operator/pkg/apis/addtoscheme_cr_v1alpha1.go
@@ -0,0 +1,10 @@
+package apis
+
+import (
+	"github.com/joshiomkarj/kanister-operator/pkg/apis/cr/v1alpha1"
+)
+
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+}
---
+++ operator/kanister-operator/pkg/apis/cr/group.go
@@ -0,0 +1,6 @@
+// Package cr contains cr API versions.
+//
+// This file ensures Go source parsers acknowledge the cr package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package cr
