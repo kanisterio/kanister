16368a3e30bd9317008946ffae40fe11dbf9a4ca
Omkar Joshi <joshiomkarj@gmail.com>
2019-08-22 23:53:59 +0530

    Resolved the conflicts and updated the license

--- pkg/customresource/customresource.go
+++ pkg/customresource/customresource.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Kasten Inc.
+// Copyright 2019 The Kanister Authors.
 // Copyright 2016 The Rook Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
