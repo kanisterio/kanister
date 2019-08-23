e992aaecfcc536b2fe9e06f5abb63ee82141f718
Thomas Manville <tom@kasten.io>
2019-08-23 16:02:50 -0700

    Revert "Stream logs from kube-task"

--- pkg/function/kube_task_test.go
+++ pkg/function/kube_task_test.go
@@ -47,96 +47,61 @@ func (s *KubeTaskSuite) TearDownSuite(c *C) {
 	}
 }
 
-func outputPhase(namespace string) crv1alpha1.BlueprintPhase {
-	return crv1alpha1.BlueprintPhase{
-		Name: "testOutput",
-		Func: "KubeTask",
-		Args: map[string]interface{}{
-			KubeTaskNamespaceArg: namespace,
-			KubeTaskImageArg:     "kanisterio/kanister-tools:0.20.0",
-			KubeTaskCommandArg: []string{
-				"sh",
-				"-c",
-				"kando output version 0.20.0",
-			},
-		},
-	}
-}
-
-func sleepPhase(namespace string) crv1alpha1.BlueprintPhase {
-	return crv1alpha1.BlueprintPhase{
-		Name: "testSleep",
-		Func: "KubeTask",
-		Args: map[string]interface{}{
-			KubeTaskNamespaceArg: namespace,
-			KubeTaskImageArg:     "ubuntu:latest",
-			KubeTaskCommandArg: []string{
-				"sleep",
-				"2",
-			},
-		},
-	}
-}
-
-func tickPhase(namespace string) crv1alpha1.BlueprintPhase {
-	return crv1alpha1.BlueprintPhase{
-		Name: "testTick",
-		Func: "KubeTask",
-		Args: map[string]interface{}{
-			KubeTaskNamespaceArg: namespace,
-			KubeTaskImageArg:     "alpine:3.10",
-			KubeTaskCommandArg: []string{
-				"sh",
-				"-c",
-				`for i in $(seq 3); do echo Tick: "${i}"; sleep 1; done`,
-			},
-		},
-	}
-}
-
-func newTaskBlueprint(phases ...crv1alpha1.BlueprintPhase) *crv1alpha1.Blueprint {
+func newTaskBlueprint(namespace string) *crv1alpha1.Blueprint {
 	return &crv1alpha1.Blueprint{
 		Actions: map[string]*crv1alpha1.BlueprintAction{
 			"test": {
-				Kind:   "StatefulSet",
-				Phases: phases,
+				Kind: "StatefulSet",
+				Phases: []crv1alpha1.BlueprintPhase{
+					{
+						Name: "testOutput",
+						Func: "KubeTask",
+						Args: map[string]interface{}{
+							KubeTaskNamespaceArg: namespace,
+							KubeTaskImageArg:     "kanisterio/kanister-tools:0.20.0",
+							KubeTaskCommandArg: []string{
+								"sh",
+								"-c",
+								"kando output version 0.20.0",
+							},
+						},
+					},
+					{
+						Name: "testSleep",
+						Func: "KubeTask",
+						Args: map[string]interface{}{
+							KubeTaskNamespaceArg: namespace,
+							KubeTaskImageArg:     "ubuntu:latest",
+							KubeTaskCommandArg: []string{
+								"sleep",
+								"2",
+							},
+						},
+					},
+				},
 			},
 		},
 	}
 }
 
 func (s *KubeTaskSuite) TestKubeTask(c *C) {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 	tp := param.TemplateParams{
 		StatefulSet: &param.StatefulSetParams{
 			Namespace: s.namespace,
 		},
 	}
-	action := "test"
-	for _, tc := range []struct {
-		bp   *crv1alpha1.Blueprint
-		outs []map[string]interface{}
-	}{
-		{
-			bp: newTaskBlueprint(outputPhase(s.namespace), sleepPhase(s.namespace), tickPhase(s.namespace)),
-			outs: []map[string]interface{}{
-				map[string]interface{}{
-					"version": "0.20.0",
-				},
-				map[string]interface{}{},
-				map[string]interface{}{},
-			},
-		},
-	} {
 
-		phases, err := kanister.GetPhases(*tc.bp, action, tp)
-		c.Assert(err, IsNil)
-		c.Assert(phases, HasLen, len(tc.outs))
-		for i, p := range phases {
-			out, err := p.Exec(ctx, *tc.bp, action, tp)
-			c.Assert(err, IsNil, Commentf("Phase %s failed", p.Name()))
-			c.Assert(out, DeepEquals, tc.outs[i])
+	action := "test"
+	bp := newTaskBlueprint(s.namespace)
+	phases, err := kanister.GetPhases(*bp, action, tp)
+	c.Assert(err, IsNil)
+	for _, p := range phases {
+		out, err := p.Exec(ctx, *bp, action, tp)
+		c.Assert(err, IsNil, Commentf("Phase %s failed", p.Name()))
+		if out != nil {
+			c.Assert(out["version"], NotNil)
 		}
 	}
 }
