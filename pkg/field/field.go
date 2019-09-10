2e7b7848b0b830b3cc86875b08ff7cd3606d401f
Julio Lopez <julio@kasten.io>
2019-09-10 12:25:06 -0700

    API for field package
    * field.Field interface definition
    * package-level functions: New, Add

---
+++ pkg/field/field.go
@@ -0,0 +1,27 @@
+package field
+
+// Field is a tuple with a string key and a value of any type
+type Field interface {
+	Key() string
+	Value() interface{}
+}
+
+// Fieldser is a collection of fields
+type Fieldser interface {
+	Fields() []Field
+}
+
+// Fields is an alias for Fieldser to make the interface friendlier. It seems
+// easier to talk about fields than fieldser(s)
+type Fields = Fieldser
+
+// New creates a new field collection with given key and value
+func New(key string, value interface{}) Fields {
+	return Add(nil, key, value)
+}
+
+// Add returns a collection with all the fields in s plus a new field with the
+// given key and value. Duplicates are not eliminated.
+func Add(s Fields, key string, value interface{}) Fields {
+	return nil
+}
