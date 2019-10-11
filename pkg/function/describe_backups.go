eb3762def364e05923416dc97dadb3c15301cb19
DeepikaDixit <deepika@kasten.io>
2019-10-10 21:09:16 -0700

    Render all output arts (#352)

--- pkg/function/describe_backups.go
+++ pkg/function/describe_backups.go
@@ -92,13 +92,21 @@ func describeBackupsPodFunc(cli kubernetes.Interface, tp param.TemplateParams, n
 			break
 		case strings.Contains(err.Error(), restic.PasswordIncorrect):
 			return map[string]interface{}{
+					DescribeBackupsSnapshotIDs:       nil,
+					DescribeBackupsFileCount:         nil,
+					DescribeBackupsSize:              nil,
 					DescribeBackupsPasswordIncorrect: "true",
+					DescribeBackupsRepoDoesNotExist:  nil,
 				},
 				nil
 
 		case strings.Contains(err.Error(), restic.RepoDoesNotExist):
 			return map[string]interface{}{
-					DescribeBackupsRepoDoesNotExist: "true",
+					DescribeBackupsSnapshotIDs:       nil,
+					DescribeBackupsFileCount:         nil,
+					DescribeBackupsSize:              nil,
+					DescribeBackupsPasswordIncorrect: nil,
+					DescribeBackupsRepoDoesNotExist:  "false",
 				},
 				nil
 		default:
