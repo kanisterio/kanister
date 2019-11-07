82d2819278f44866ff554483914076fa773e6179
Pavan Navarathna Devaraj <pdevaraj@Pavans-MacBook-Pro.local>
2019-11-06 18:16:05 -0800

    Modify DescribeBackups test

--- pkg/function/data_test.go
+++ pkg/function/data_test.go
@@ -179,7 +179,7 @@ func newDescribeBackupsBlueprint() *crv1alpha1.Blueprint {
 						Name: "testDescribeBackups",
 						Func: "DescribeBackups",
 						Args: map[string]interface{}{
-							DescribeBackupsArtifactPrefixArg: "{{ .Profile.Location.Bucket }}/{{ .Profile.Location.Prefix }}",
+							DescribeBackupsArtifactPrefixArg: "{{ .Profile.Location.Prefix }}",
 							DescribeBackupsEncryptionKeyArg:  "{{ .Secrets.backupKey.Data.password | toString }}",
 						},
 					},
