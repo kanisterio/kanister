b1df034d614baae8155ce0e453cdffae39050052
Vivek Singh <vsingh.ggits.2010@gmail.com>
2019-11-30 15:41:50 +0530

    Remove unncessary package alias

--- pkg/testing/integration_test.go
+++ pkg/testing/integration_test.go
@@ -21,7 +21,7 @@ import (
 
 	"github.com/pkg/errors"
 	. "gopkg.in/check.v1"
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
