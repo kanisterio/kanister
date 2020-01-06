6fee94bae2b09cfe56da8f931409e7316f9478a2
Prasad Ghangal <prasad.ghangal@gmail.com>
2020-01-06 12:14:47 +0530

    Add helper method FindSecurityGroups from instanceID Refactor RDS create and update methods to support sgID list
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

--- pkg/app/rds_postgres.go
+++ pkg/app/rds_postgres.go
@@ -54,7 +54,7 @@ type RDSPostgresDB struct {
 	secretKey       string
 	region          string
 	sessionToken    string
-	securityGroupID string
+	securityGroupID *string
 	sqlDB           *sql.DB
 }
 
@@ -123,7 +123,7 @@ func (pdb *RDSPostgresDB) Install(ctx context.Context, ns string) error {
 	if err != nil {
 		return err
 	}
-	pdb.securityGroupID = *sg.GroupId
+	pdb.securityGroupID = sg.GroupId
 
 	// Add ingress rule
 	log.Info().Print("Adding ingress rule to security group.", field.M{"app": pdb.name})
@@ -140,7 +140,7 @@ func (pdb *RDSPostgresDB) Install(ctx context.Context, ns string) error {
 
 	// Create RDS instance
 	log.Info().Print("Creating RDS instance.", field.M{"app": pdb.name, "id": pdb.id})
-	_, err = rdsCli.CreateDBInstance(ctx, 20, "db.t2.micro", pdb.id, "postgres", pdb.username, pdb.password, pdb.securityGroupID)
+	_, err = rdsCli.CreateDBInstance(ctx, 20, "db.t2.micro", pdb.id, "postgres", pdb.username, pdb.password, []*string{pdb.securityGroupID})
 	if err != nil {
 		return err
 	}
