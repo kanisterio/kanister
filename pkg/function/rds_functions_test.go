0f930b6c994cef9d5753ac1909c9d0f23e42169f
Vivek Singh <vsingh.ggits.2010@gmail.com>
2020-01-30 20:02:04 +0530

    Add test for rds functions (#497)
    * Add test for rds functions
    
    * Refactor and rebase with master
    
    * Add neg. test
    
    * Remove unused field
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ pkg/function/rds_functions_test.go
@@ -0,0 +1,111 @@
+// Copyright 2019 The Kanister Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package function
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/kanisterio/kanister/pkg/param"
+	. "gopkg.in/check.v1"
+)
+
+type RDSFunctionsTest struct{}
+
+var _ = Suite(&RDSFunctionsTest{})
+
+func (s *RDSFunctionsTest) TestPrepareCommand(c *C) {
+	testCases := []struct {
+		name         string
+		dbEngine     RDSDBEngine
+		dbList       []string
+		action       RDSAction
+		dbEndpoint   string
+		username     string
+		password     string
+		backupPrefix string
+		backupID     string
+		errChecker   Checker
+		tp           param.TemplateParams
+		command      []string
+	}{
+		{
+			name:         "PostgreS restore command",
+			dbEngine:     PostgrSQLEngine,
+			action:       RestoreAction,
+			dbEndpoint:   "db-endpoint",
+			username:     "dummy-user",
+			password:     "secret-pass",
+			backupPrefix: "/backup/postgres-backup",
+			backupID:     "backup-id",
+			errChecker:   IsNil,
+			dbList:       []string{"template1"},
+			command: []string{"bash", "-o", "errexit", "-o", "pipefail", "-c",
+				fmt.Sprintf(`
+		export PGHOST=%s
+		kando location pull --profile '%s' --path "%s" - | gunzip -c -f | psql -q -U "${PGUSER}" %s
+		`, "db-endpoint", "null", fmt.Sprintf("%s/%s", "/backup/postgres-backup", "backup-id"), []string{"template1"}[0]),
+			},
+		},
+		{
+			name:         "PostgreS backup command",
+			dbEngine:     PostgrSQLEngine,
+			action:       BackupAction,
+			dbEndpoint:   "db-endpoint",
+			username:     "dummy-user",
+			password:     "secret-pass",
+			backupPrefix: "/backup/postgres-backup",
+			backupID:     "backup-id",
+			errChecker:   IsNil,
+			dbList:       []string{"template1"},
+			command: []string{"bash", "-o", "errexit", "-o", "pipefail", "-c",
+				fmt.Sprintf(`
+			export PGHOST=%s
+			BACKUP_PREFIX=%s
+			BACKUP_ID=%s
+
+			mkdir /backup
+			dblist=( %s )
+			for db in "${dblist[@]}";
+			  do echo "backing up $db db" && pg_dump $db -C --inserts > /backup/$db.sql;
+			done
+			tar -zc backup | kando location push --profile '%s' --path "${BACKUP_PREFIX}/${BACKUP_ID}" -
+			kando output %s ${BACKUP_ID}`,
+					"db-endpoint", "/backup/postgres-backup", "backup-id", strings.Join([]string{"template1"}, " "), "null", ExportRDSSnapshotToLocBackupID),
+			},
+		},
+		{
+			name:         "PostgreS backup command",
+			dbEngine:     "MySQLDBEngine",
+			action:       BackupAction,
+			dbEndpoint:   "db-endpoint",
+			username:     "dummy-user",
+			password:     "secret-pass",
+			backupPrefix: "/backup/postgres-backup",
+			backupID:     "backup-id",
+			errChecker:   NotNil,
+			dbList:       []string{"template1"},
+			command:      nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		outCommand, _, err := prepareCommand(context.Background(), tc.dbEngine, tc.action, tc.dbEndpoint, tc.username, tc.password, tc.dbList, tc.backupPrefix, tc.backupID, tc.tp.Profile)
+
+		c.Check(err, tc.errChecker, Commentf("Case %s failed", tc.name))
+		c.Assert(outCommand, DeepEquals, tc.command)
+	}
+}
