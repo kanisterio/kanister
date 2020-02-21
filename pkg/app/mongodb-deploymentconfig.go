6a11927a715028004c905bb4988efe85ecaf869c
Vivek Singh <vsingh.ggits.2010@gmail.com>
2020-02-21 19:30:26 +0530

    Fix CI issue

--- pkg/app/mongodb-deploymentconfig.go
+++ pkg/app/mongodb-deploymentconfig.go
@@ -126,7 +126,6 @@ func (mongo *MongoDBDepConfig) Ping(ctx context.Context) error {
 }
 
 func (mongo *MongoDBDepConfig) Insert(ctx context.Context) error {
-
 	log.Print("Inserting documents into collection.", field.M{"app": mongo.name})
 	insertCMD := []string{"sh", "-c", fmt.Sprintf("mongo admin --authenticationDatabase admin -u %s -p $MONGODB_ADMIN_PASSWORD --quiet --eval \"db.restaurants.insert({'_id': '%s','name' : 'Tom', 'cuisine' : 'Hawaiian', 'id' : '8675309'})\"", mongo.user, uuid.New())}
 	_, stderr, err := mongo.execCommand(ctx, insertCMD)
