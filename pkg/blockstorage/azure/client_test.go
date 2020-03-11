43db7c52df399b8134237c29ba9981ed66398c0e
SupriyaKasten <supriya@kasten.io>
2020-03-10 17:22:43 -0700

    undo changes to client_test

--- pkg/blockstorage/azure/client_test.go
+++ pkg/blockstorage/azure/client_test.go
@@ -41,8 +41,6 @@ func (s *ClientSuite) TestClient(c *C) {
 	config[blockstorage.AzureCientID] = envconfig.GetEnvOrSkip(c, blockstorage.AzureCientID)
 	config[blockstorage.AzureClentSecret] = envconfig.GetEnvOrSkip(c, blockstorage.AzureClentSecret)
 	config[blockstorage.AzureResurceGroup] = envconfig.GetEnvOrSkip(c, blockstorage.AzureResurceGroup)
-	config[blockstorage.AzureMigrateStorageAccount] = envconfig.GetEnvOrSkip(c, blockstorage.AzureMigrateStorageAccount)
-	config[blockstorage.AzureMigrateStorageKey] = envconfig.GetEnvOrSkip(c, blockstorage.AzureMigrateStorageKey)
 	azCli, err := NewClient(context.Background(), config)
 	c.Assert(err, IsNil)
 
