00b267d8334c924fdaa90754bf4e28347746e9a5
Vaibhav Kamra <vkamra@kasten.io>
2020-04-15 21:49:14 -0700

    Kanister docs update to version 0.29.0 (#647)
    * Kanister docs update to version 0.29.0
    
    * Skip Elastic integration test
    
    Co-authored-by: Shippable Build <build@shippable.com>
    Co-authored-by: Julio López <julio@kasten.io>
    Co-authored-by: Tom Manville <tom@kasten.io>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pkg/app/mongodb.go
+++ pkg/app/mongodb.go
@@ -62,7 +62,7 @@ func NewMongoDB(name string) App {
 			Values: map[string]string{
 				"replicaSet.enabled": "true",
 				"image.repository":   "kanisterio/mongodb",
-				"image.tag":          "0.28.0",
+				"image.tag":          "0.29.0",
 			},
 		},
 	}
--- pkg/function/export_rds_snapshot_location.go
+++ pkg/function/export_rds_snapshot_location.go
@@ -60,7 +60,7 @@ const (
 	BackupAction  RDSAction = "backup"
 	RestoreAction RDSAction = "restore"
 
-	postgresToolsImage = "kanisterio/postgres-kanister-tools:0.28.0"
+	postgresToolsImage = "kanisterio/postgres-kanister-tools:0.29.0"
 )
 
 type exportRDSSnapshotToLocationFunc struct{}
