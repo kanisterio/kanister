16895f1a35534efd4e2cc1f9f65f8f40f11a94ee
Sirish Bathina <sirish@kasten.io>
2020-04-20 17:21:11 -0700

    fixing nil case

--- pkg/blockstorage/azure/azuredisk.go
+++ pkg/blockstorage/azure/azuredisk.go
@@ -496,7 +496,7 @@ func (s *adStorage) VolumeCreateFromSnapshot(ctx context.Context, snapshot block
 			}
 		}
 	}
-	if createDisk.Sku.Name == "" {
+	if createDisk.Sku == nil || createDisk.Sku.Name == "" {
 		return nil, errors.New(fmt.Sprintf("Invalid disk skuName %s", snapshot.Volume.VolumeType))
 	}
 	result, err := s.azCli.DisksClient.CreateOrUpdate(ctx, s.azCli.ResourceGroup, diskName, createDisk)
