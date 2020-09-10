77367945213358e84ea40db010b4cc78d6f58f82
Arush Salil <me@aru.sh>
2020-09-10 20:25:14 +0530

    Update Cassandra chart version to latest (#755)
    * Remove version field to always use latest
    
    Signed-off-by: Arush Salil <me@aru.sh>
    
    * change helm repo and url to bitnami
    
    Signed-off-by: Arush Salil <me@aru.sh>
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pkg/app/cassandra.go
+++ pkg/app/cassandra.go
@@ -47,15 +47,15 @@ type CassandraInstance struct {
 }
 
 // NewCassandraInstance returns new cassandra application
+// Last tested working version "0.13.3"
 func NewCassandraInstance(name string) App {
 	return &CassandraInstance{
 		name: name,
 		chart: helm.ChartInfo{
 			Release:  appendRandString(name),
-			RepoURL:  helm.IncubatorRepoURL,
+			RepoURL:  helm.BitnamiRepoURL,
+			RepoName: helm.BitnamiRepoName,
 			Chart:    "cassandra",
-			RepoName: helm.IncubatorRepoName,
-			Version:  "0.13.3",
 			Values: map[string]string{
 				"image.repo":          "kanisterio/cassandra",
 				"image.tag":           "0.34.0",
