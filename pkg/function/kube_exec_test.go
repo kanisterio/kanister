e17a667a4fe2c8c50d1ff5b4515a5152238b0080
Vaibhav Kamra <vkamra@kasten.io>
2020-09-19 00:39:06 -0700

    Kanister docs update to version 0.36.0 (#774)
    Co-authored-by: Shippable Build <build@shippable.com>

--- pkg/function/kube_exec_test.go
+++ pkg/function/kube_exec_test.go
@@ -178,11 +178,11 @@ func (s *KubeExecTest) TestParseLogAndCreateOutput(c *C) {
 		errChecker Checker
 		outChecker Checker
 	}{
-		{"###Phase-output###: {\"key\":\"version\",\"value\":\"0.35.0\"}", map[string]interface{}{"version": "0.35.0"}, IsNil, NotNil},
-		{"###Phase-output###: {\"key\":\"version\",\"value\":\"0.35.0\"}\n###Phase-output###: {\"key\":\"path\",\"value\":\"/backup/path\"}",
-			map[string]interface{}{"version": "0.35.0", "path": "/backup/path"}, IsNil, NotNil},
-		{"Random message ###Phase-output###: {\"key\":\"version\",\"value\":\"0.35.0\"}", map[string]interface{}{"version": "0.35.0"}, IsNil, NotNil},
-		{"Random message with newline \n###Phase-output###: {\"key\":\"version\",\"value\":\"0.35.0\"}", map[string]interface{}{"version": "0.35.0"}, IsNil, NotNil},
+		{"###Phase-output###: {\"key\":\"version\",\"value\":\"0.36.0\"}", map[string]interface{}{"version": "0.36.0"}, IsNil, NotNil},
+		{"###Phase-output###: {\"key\":\"version\",\"value\":\"0.36.0\"}\n###Phase-output###: {\"key\":\"path\",\"value\":\"/backup/path\"}",
+			map[string]interface{}{"version": "0.36.0", "path": "/backup/path"}, IsNil, NotNil},
+		{"Random message ###Phase-output###: {\"key\":\"version\",\"value\":\"0.36.0\"}", map[string]interface{}{"version": "0.36.0"}, IsNil, NotNil},
+		{"Random message with newline \n###Phase-output###: {\"key\":\"version\",\"value\":\"0.36.0\"}", map[string]interface{}{"version": "0.36.0"}, IsNil, NotNil},
 		{"###Phase-output###: Invalid message", nil, NotNil, IsNil},
 		{"Random message", nil, IsNil, IsNil},
 	} {
