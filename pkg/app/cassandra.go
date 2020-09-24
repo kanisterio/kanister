6845f14e61763bb8d553f455dcad3aed90536ba0
Ankit Jain <ankitjain.meone@gmail.com>
2020-09-24 18:43:57 +0530

    Remore cassandra changes

--- pkg/app/cassandra.go
+++ pkg/app/cassandra.go
@@ -47,15 +47,15 @@ type CassandraInstance struct {
 }
 
 // NewCassandraInstance returns new cassandra application
-// Last tested working version "0.13.3"
 func NewCassandraInstance(name string) App {
 	return &CassandraInstance{
 		name: name,
 		chart: helm.ChartInfo{
 			Release:  appendRandString(name),
-			RepoURL:  helm.BitnamiRepoURL,
-			RepoName: helm.BitnamiRepoName,
+			RepoURL:  helm.IncubatorRepoURL,
 			Chart:    "cassandra",
+			RepoName: helm.IncubatorRepoName,
+			Version:  "0.13.3",
 			Values: map[string]string{
 				"image.repo":          "kanisterio/cassandra",
 				"image.tag":           "0.36.0",
