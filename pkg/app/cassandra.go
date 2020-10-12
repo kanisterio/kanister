f7292c728257d51ed9c57c94e4f1adf653b581f1
Ankit Jain <ankitjain.meone@gmail.com>
2020-10-12 16:03:21 +0530

    Revert temporary changes

--- pkg/app/cassandra.go
+++ pkg/app/cassandra.go
@@ -56,8 +56,8 @@ func NewCassandraInstance(name string) App {
 			Chart:    "cassandra",
 			RepoName: helm.BitnamiRepoName,
 			Values: map[string]string{
-				"image.repository":     "ankitjain235/cassandra",
-				"image.tag":            "test",
+				"image.repository":     "kanisterio/cassandra",
+				"image.tag":            "0.36.0",
 				"cluster.replicaCount": "1",
 			},
 		},
