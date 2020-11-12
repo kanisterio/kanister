7661ec61fbdc116a1cb4c5db114e1cbe549190f7
PrasadG193 <prasad.ghangal@gmail.com>
2020-11-13 00:07:02 +0530

    Kanister docs update to version 0.43.0

--- pkg/app/mongodb.go
+++ pkg/app/mongodb.go
@@ -62,7 +62,7 @@ func NewMongoDB(name string) App {
 			Values: map[string]string{
 				"architecture":     "replicaset",
 				"image.repository": "kanisterio/mongodb",
-				"image.tag":        "0.41.0",
+				"image.tag":        "0.43.0",
 			},
 		},
 	}
--- scripts/get.sh
+++ scripts/get.sh
@@ -140,7 +140,7 @@ cleanup() {
 }
 
 main() {
-    version="${1:-"0.41.0"}"
+    version="${1:-"0.43.0"}"
     initArch
     initOS
     verifySupported
