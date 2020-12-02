400fcd805a155bbe081465cb13ef349077af335c
Rahul M Chheda <rchheda@infracloud.io>
2020-12-02 15:59:25 +0530

    Remove un-necessary changes
    Signed-off-by: Rahul M Chheda <rchheda@infracloud.io>

--- build/test.sh
+++ build/test.sh
@@ -25,30 +25,30 @@ export GO111MODULE=on
 
 TARGETS=$(for d in "$@"; do echo ./$d/...; done)
 
-# echo -n "Checking gofmt: "
-# ERRS=$(find "$@" -type f -name \*.go | xargs gofmt -l 2>&1 || true)
-# if [ -n "${ERRS}" ]; then
-#     echo "FAIL - the following files need to be gofmt'ed:"
-#     for e in ${ERRS}; do
-#         echo "    $e"
-#     done
-#     echo
-#     exit 1
-# fi
-# echo "PASS"
-# echo
+echo -n "Checking gofmt: "
+ERRS=$(find "$@" -type f -name \*.go | xargs gofmt -l 2>&1 || true)
+if [ -n "${ERRS}" ]; then
+    echo "FAIL - the following files need to be gofmt'ed:"
+    for e in ${ERRS}; do
+        echo "    $e"
+    done
+    echo
+    exit 1
+fi
+echo "PASS"
+echo
 
-# echo -n "Checking go vet: "
-# ERRS=$(go vet ${TARGETS} 2>&1 || true)
-# if [ -n "${ERRS}" ]; then
-#     echo "FAIL"
-#     echo "${ERRS}"
-#     echo
-#     # TODO: Renable govet. Currently generated code fails to pass go vet. report,
-#     # but don't exit on failures.
-#     #exit 1
-# fi
-# echo
+echo -n "Checking go vet: "
+ERRS=$(go vet ${TARGETS} 2>&1 || true)
+if [ -n "${ERRS}" ]; then
+    echo "FAIL"
+    echo "${ERRS}"
+    echo
+    # TODO: Renable govet. Currently generated code fails to pass go vet. report,
+    # but don't exit on failures.
+    #exit 1
+fi
+echo
 
 echo "Running tests:"
 go test -v -installsuffix "static" -i ${TARGETS}
