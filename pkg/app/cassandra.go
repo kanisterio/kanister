e31f99fc926f2c6d85e7aaa900386867f12c094a
Shippable Build <build@shippable.com>
2020-12-11 04:55:15 +0000

    Kanister docs update to version 0.45.0

--- pkg/app/cassandra.go
+++ pkg/app/cassandra.go
@@ -58,7 +58,7 @@ func NewCassandraInstance(name string) App {
 			Values: map[string]string{
 				"image.registry":       "ghcr.io",
 				"image.repository":     "kanisterio/cassandra",
-				"image.tag":            "0.44.0",
+				"image.tag":            "0.45.0",
 				"image.pullPolicy":     "Always",
 				"cluster.replicaCount": "1",
 			},
