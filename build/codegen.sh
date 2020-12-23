f0aa2d08fbdbc8b266e0a6960be15fef266f0446
Ankit Jain <ankitjain.meone@gmail.com>
2020-12-23 16:46:18 +0530

    Update codegen to fix GOPATH issue

--- build/codegen.sh
+++ build/codegen.sh
@@ -22,7 +22,7 @@ set -o xtrace
 
 export GO111MODULE=on
 go mod download
-execDir="$GOPATH/pkg/mod/k8s.io/code-generator@$(go list -f '{{.Version}}' -m k8s.io/code-generator)"
+execDir="/root/go/pkg/mod/k8s.io/code-generator@$(go list -f '{{.Version}}' -m k8s.io/code-generator)"
 chmod +x "${execDir}"/generate-groups.sh
 "${execDir}"/generate-groups.sh                         \
   all                                        \
