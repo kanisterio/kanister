b9a03349715dfc9771b1dcd169b2a47e6108b783
Shippable Build <build@shippable.com>
2021-01-15 21:41:40 +0000

    Kanister docs update to version 0.49.0

--- pkg/function/utils.go
+++ pkg/function/utils.go
@@ -26,7 +26,7 @@ import (
 const (
 	// FunctionOutputVersion returns version
 	FunctionOutputVersion     = "version"
-	kanisterToolsImage        = "ghcr.io/kanisterio/kanister-tools:0.48.0"
+	kanisterToolsImage        = "ghcr.io/kanisterio/kanister-tools:0.49.0"
 	kanisterToolsImageEnvName = "KANISTER_TOOLS"
 )
 
