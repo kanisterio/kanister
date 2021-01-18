37a56375b710ce22eb54ef87adb97f5d669014ba
A-kanksh-a <akankshakumari393@gmail.com>
2021-01-18 17:15:43 +0530

    customized connector

--- examples/kafka/monitorconnect.sh
+++ examples/kafka/monitorconnect.sh
@@ -2,17 +2,18 @@
 echo "===========================Monitoring started==================================="
 sleep 60
 export flushsize=`cat /tmp/config/s3config.properties | grep "flush.size=" | awk -F "=" '{print $2}'`
+export group="connect-${connectorName}"
 #export pid="$(lsof -t -i:8083)"
 export elapsedtime="$(ps -e -o "pid,etimes,command" |awk -v processid=$pid '{if($1==processid) print $2}')"
-export lag="$(/bin/kafka-consumer-groups --bootstrap-server "$bootstrapServer" --describe --group connect-kanister-kafka-S3SinkConnector| awk 'BEGIN{maxLag=   0}{if ($6>0+maxLag) maxLag=$6} END{print maxLag}')"
-echo "========================== MaxTime=$timeinSeconds, flushsize=$flushsize, pid=$pid, elapsedtime=$elapsedtime, lag=$lag, ==============================="
+export lag="$(/bin/kafka-consumer-groups --bootstrap-server "$bootstrapServer" --describe --group "$group"| awk 'BEGIN{maxLag=   0}{if ($6>0+maxLag) maxLag=$6} END{print maxLag}')"
+echo "==========================Group=$group, MaxTime=$timeinSeconds, flushsize=$flushsize, pid=$pid, elapsedtime=$elapsedtime, lag=$lag, ==============================="
 if [ $lag = "LAG" ]
 then
     export lag=0
 fi
 while [[ "$lag" -gt "$flushsize"  && "$elapsedtime" -lt "$timeinSeconds" ]]
 do
-lag="$(/bin/kafka-consumer-groups --bootstrap-server "$bootstrapServer" --describe --group connect-kanister-kafka-S3SinkConnector| awk 'BEGIN{maxLag=   0}{if ($6>0+maxLag) maxLag=$6} END{print maxLag}')"
+lag="$(/bin/kafka-consumer-groups --bootstrap-server "$bootstrapServer" --describe --group "$group"| awk 'BEGIN{maxLag=   0}{if ($6>0+maxLag) maxLag=$6} END{print maxLag}')"
 elapsedtime="$(ps -e -o "pid,etimes,command" |awk -v processid=$pid '{if($1==processid) print $2}')"
 echo "========================== lag = $lag , elapsedtime = $elapsedtime ======================================="
 sleep 2
