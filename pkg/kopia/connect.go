23448c10ef28c4a6e3ab0397cade3fbc7109529a
Prasad Ghangal <prasad.ghangal@gmail.com>
2021-02-06 03:58:46 +0530

    Add kopia server connect helper methods (#888)
    * Add kopia server connect helper methods
    
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>
    
    * Add licence headers
    
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>
    
    * Update connect options
    
    Co-authored-by: Pavan Navarathna <pavan@kasten.io>

---
+++ pkg/kopia/connect.go
@@ -0,0 +1,115 @@
+// Copyright 2021 The Kanister Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kopia
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/jpillora/backoff"
+	"github.com/kopia/kopia/repo"
+	"github.com/kopia/kopia/repo/content"
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/kanisterio/kanister/pkg/poll"
+)
+
+const (
+	defaultConnectMaxListCacheDuration time.Duration = time.Second * 600
+	defaultConnectPersistCredentials                 = true
+)
+
+// ConnectToAPIServer connects to the Kopia API server running at the given address
+func ConnectToAPIServer(
+	ctx context.Context,
+	cli kubernetes.Interface,
+	tlsCertSecret,
+	userPassphraseSecret *corev1.Secret,
+	hostname,
+	serverAddress,
+	username string,
+) error {
+	// Extra fingerprint from the TLS Certificate secret
+	fingerprint, err := ExtractFingerprintFromCertSecret(ctx, cli, tlsCertSecret.GetName(), tlsCertSecret.GetNamespace())
+	if err != nil {
+		return errors.Wrap(err, "Failed to extract fingerprint from Kopia API Server Certificate Secret")
+	}
+
+	// Extract user passphrase from the secret
+	passphrase, ok := userPassphraseSecret.Data[hostname]
+	if !ok {
+		return errors.Errorf("Failed to extract client passphrase from secret. Secret: %s", userPassphraseSecret.GetName())
+	}
+
+	serverInfo := &repo.APIServerInfo{
+		BaseURL:                             serverAddress,
+		TrustedServerCertificateFingerprint: fingerprint,
+	}
+
+	opts := &repo.ConnectOptions{
+		PersistCredentials: defaultConnectPersistCredentials,
+		CachingOptions: content.CachingOptions{
+			CacheDirectory:            defaultCacheDirectory,
+			MaxCacheSizeBytes:         int64(defaultDataStoreGeneralContentCacheSizeMB << 20),
+			MaxMetadataCacheSizeBytes: int64(defaultDataStoreGeneralMetadataCacheSizeMB << 20),
+			MaxListCacheDurationSec:   int(defaultConnectMaxListCacheDuration.Seconds()),
+		},
+		ClientOptions: repo.ClientOptions{
+			Hostname: hostname,
+			Username: username,
+		},
+	}
+
+	err = poll.WaitWithBackoff(ctx, backoff.Backoff{
+		Factor: 2,
+		Jitter: false,
+		Min:    100 * time.Millisecond,
+		Max:    180 * time.Second,
+	}, func(c context.Context) (bool, error) {
+		if err := repo.ConnectAPIServer(ctx, defaultConfigFilePath, serverInfo, string(passphrase), opts); err != nil {
+			return false, nil
+		}
+		return true, nil
+	})
+	if err != nil {
+		return errors.Wrap(err, "Failed to backup data to Kopia API server")
+	}
+	return nil
+}
+
+// OpenRepository opens the connected Kopia repository
+func OpenRepository(ctx context.Context) (repo.Repository, error) {
+	if _, err := os.Stat(defaultConfigFilePath); os.IsNotExist(err) {
+		return nil, errors.New("Failed find Kopia configuration file")
+	}
+
+	password, ok := repo.GetPersistedPassword(ctx, defaultConfigFilePath)
+	if !ok || password == "" {
+		return nil, errors.New("Failed to retrieve Kopia client passphrase")
+	}
+
+	r, err := repo.Open(ctx, defaultConfigFilePath, password, &repo.Options{})
+	if os.IsNotExist(err) {
+		return nil, errors.New("Failed to find Kopia repository, not connected to any repository")
+	}
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to open Kopia repository")
+	}
+
+	return r, nil
+}
---
+++ pkg/kopia/utils.go
@@ -0,0 +1,89 @@
+// Copyright 2021 The Kanister Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kopia
+
+import (
+	"context"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"encoding/pem"
+
+	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+const (
+	// defaultConfigFilePath is the file which contains kopia repo config
+	defaultConfigFilePath = "/tmp/kopia-repository.config"
+
+	// defaultCacheDirectory is the directory where kopia content cache is created
+	defaultCacheDirectory = "/tmp/kopia-cache"
+
+	// defaultDataStoreGeneralContentCacheSizeMB is the default content cache size for general command workloads
+	defaultDataStoreGeneralContentCacheSizeMB = 0
+
+	// defaultDataStoreGeneralMetadataCacheSizeMB is the default metadata cache size for general command workloads
+	defaultDataStoreGeneralMetadataCacheSizeMB = 500
+
+	// tlsCertificateKey represents the key used to fetch the certificate
+	// from the secret.
+	tlsCertificateKey = "tls.crt"
+)
+
+// ExtractFingerprintFromCertSecret extracts the fingerprint from the given certificate secret
+func ExtractFingerprintFromCertSecret(ctx context.Context, cli kubernetes.Interface, secretName, secretNamespace string) (string, error) {
+	secret, err := cli.CoreV1().Secrets(secretNamespace).Get(ctx, secretName, metav1.GetOptions{})
+	if err != nil {
+		return "", errors.Wrapf(err, "Failed to get Certificate Secret. Secret: %s", secretName)
+	}
+
+	certBytes, err := json.Marshal(secret.Data[tlsCertificateKey])
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to marshal Certificate Secret Data")
+	}
+
+	var certString string
+	if err := json.Unmarshal([]byte(certBytes), &certString); err != nil {
+		return "", errors.Wrap(err, "Failed to unmarshal Certificate Secret Data")
+	}
+
+	decodedCertData, err := base64.StdEncoding.DecodeString(certString)
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to decode Certificate Secret Data")
+	}
+
+	return extractFingerprintFromSliceOfBytes(decodedCertData)
+}
+
+// extractFingerprintFromSliceOfBytes extracts the fingeprint from the
+// certificate data provided in slice of bytes (default type for secret.Data)
+func extractFingerprintFromSliceOfBytes(pemData []byte) (string, error) {
+	block, rest := pem.Decode([]byte(pemData))
+	if block == nil || len(rest) > 0 {
+		return "", errors.New("Failed to PEM Decode Kopia API Server Certificate Secret Data")
+	}
+
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to parse X509 Kopia API Server Certificate Secret Data")
+	}
+
+	fingerprint := sha256.Sum256(cert.Raw)
+	return hex.EncodeToString(fingerprint[:]), nil
+}
