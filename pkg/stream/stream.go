206c79a8704c3855ee3a412e8cb05186dfb4c8a0
Pavan Navarathna Devaraj <pavan@kasten.io>
2021-03-17 14:34:10 -0700

    Remove unused code from package

--- pkg/stream/stream.go
+++ pkg/stream/stream.go
@@ -16,18 +16,14 @@ package stream
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"path/filepath"
-	"time"
 
-	"github.com/kopia/kopia/fs"
-	"github.com/kopia/kopia/repo"
 	"github.com/kopia/kopia/snapshot"
-	"github.com/kopia/kopia/snapshot/policy"
 	"github.com/kopia/kopia/snapshot/snapshotfs"
 	"github.com/pkg/errors"
 
+	kankopia "github.com/kanisterio/kanister/pkg/kopia"
 	"github.com/kanisterio/kanister/pkg/virtualfs"
 )
 
@@ -38,7 +34,7 @@ const (
 
 // Push streams data to object store by reading it from the given endpoint into an in-memory filesystem
 func Push(ctx context.Context, configFile, dirPath, filePath, password, sourceEndpoint string) error {
-	rep, err := OpenKopiaRepository(ctx, configFile, password)
+	rep, err := kankopia.OpenRepository(ctx, configFile, password, "kanister stream push")
 	if err != nil {
 		return errors.Wrap(err, "Failed to open kopia repository")
 	}
@@ -66,107 +62,5 @@ func Push(ctx context.Context, configFile, dirPath, filePath, password, sourceEn
 	}
 
 	// Create a kopia snapshot
-	return SnapshotSource(ctx, rep, u, sourceInfo, root)
-}
-
-// OpenKopiaRepository connects to the kopia repository based on the config
-// NOTE: This assumes that `kopia repository connect` has been already run on the machine
-func OpenKopiaRepository(ctx context.Context, configFile, password string) (repo.RepositoryWriter, error) {
-	repoConfig := repositoryConfigFileName(configFile)
-	if _, err := os.Stat(repoConfig); os.IsNotExist(err) {
-		return nil, errors.New("Failed find kopia configuration file")
-	}
-
-	r, err := repo.Open(ctx, repoConfig, password, &repo.Options{})
-	if os.IsNotExist(err) {
-		return nil, errors.New("Failed to find kopia repository, use `kopia repository create` or kopia repository connect` if already created")
-	}
-
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to open kopia repository")
-	}
-
-	rw, err := r.NewWriter(ctx, repo.WriteSessionOptions{
-		Purpose: "kanister stream",
-	})
-
-	return rw, errors.Wrap(err, "Failed to open kopia repository writer")
-}
-
-func repositoryConfigFileName(configFile string) string {
-	if configFile != "" {
-		return configFile
-	}
-	return filepath.Join(os.Getenv("HOME"), ".config", "kopia", "repository.config")
-}
-
-// SnapshotSource creates and uploads a kopia snapshot to the given repository
-func SnapshotSource(ctx context.Context, rep repo.RepositoryWriter, u *snapshotfs.Uploader, sourceInfo snapshot.SourceInfo, rootDir fs.Entry) error {
-	fmt.Printf("Snapshotting %v ...\n", sourceInfo)
-
-	t0 := time.Now()
-
-	previous, err := findPreviousSnapshotManifest(ctx, rep, sourceInfo, nil)
-	if err != nil {
-		return errors.Wrap(err, "Failed to find previous kopia manifests")
-	}
-
-	policyTree, err := policy.TreeForSource(ctx, rep, sourceInfo)
-	if err != nil {
-		return errors.Wrap(err, "Failed to get kopia policy tree")
-	}
-
-	manifest, err := u.Upload(ctx, rootDir, policyTree, sourceInfo, previous...)
-	if err != nil {
-		return errors.Wrap(err, "Failed to upload the kopia snapshot")
-	}
-
-	manifest.Description = snapshotDescription
-
-	snapID, err := snapshot.SaveSnapshot(ctx, rep, manifest)
-	if err != nil {
-		return errors.Wrap(err, "Failed to save kopia manifest")
-	}
-
-	_, err = policy.ApplyRetentionPolicy(ctx, rep, sourceInfo, true)
-	if err != nil {
-		return errors.Wrap(err, "Failed to apply kopia retention policy")
-	}
-
-	if ferr := rep.Flush(ctx); ferr != nil {
-		return errors.Wrap(ferr, "Failed to flush kopia repository")
-	}
-
-	fmt.Printf("\nCreated snapshot with root %v and ID %v in %v\n", manifest.RootObjectID(), snapID, time.Since(t0).Truncate(time.Second))
-
-	return err
-}
-
-// findPreviousSnapshotManifest returns the list of previous snapshots for a given source,
-// including last complete snapshot
-func findPreviousSnapshotManifest(ctx context.Context, rep repo.Repository, sourceInfo snapshot.SourceInfo, noLaterThan *time.Time) ([]*snapshot.Manifest, error) {
-	man, err := snapshot.ListSnapshots(ctx, rep, sourceInfo)
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to list previous kopia snapshots")
-	}
-
-	// find latest complete snapshot
-	var previousComplete *snapshot.Manifest
-	var result []*snapshot.Manifest
-
-	for _, p := range man {
-		if noLaterThan != nil && p.StartTime.After(*noLaterThan) {
-			continue
-		}
-
-		if p.IncompleteReason == "" && (previousComplete == nil || p.StartTime.After(previousComplete.StartTime)) {
-			previousComplete = p
-		}
-	}
-
-	if previousComplete != nil {
-		result = append(result, previousComplete)
-	}
-
-	return result, nil
+	return kankopia.SnapshotSource(ctx, rep, u, sourceInfo, root, snapshotDescription)
 }
