37d272ffd878145029e3c908c21928ec78a292c1
Pavan Navarathna <pavan@kasten.io>
2021-04-06 15:39:40 -0700

    Refactor kopia repository and snapshot helpers (#934)
    * Remove unused code from package
    
    * Refactor and add code into new package
    
    * Refactor connect and open repo helpers
    
    * More refactoring
    
    * Bump a CI build
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pkg/kopia/register.go
+++ pkg/kopia/register.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The Kanister Authors.
+// Copyright 2021 The Kanister Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package stream
+package kopia
 
 import (
 	// Register supported blob storage providers
