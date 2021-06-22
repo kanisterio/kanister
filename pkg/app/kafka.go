53795d01557dcfc32bcc0ffd298877e9326020d8
A-kanksh-a <akankshakumari393@gmail.com>
2021-06-22 17:23:46 +0530

    lint fixed

--- pkg/app/kafka.go
+++ pkg/app/kafka.go
@@ -165,7 +165,7 @@ func (kc *KafkaCluster) Install(ctx context.Context, namespace string) error {
 
 func (kc *KafkaCluster) GetClusterScopedResources(ctx context.Context) []crv1alpha1.ObjectReference {
 	return []crv1alpha1.ObjectReference{
-	// ClusterRoles
+		// ClusterRoles
 		{
 			APIVersion: "v1",
 			Group:      "rbac.authorization.k8s.io",
@@ -202,7 +202,7 @@ func (kc *KafkaCluster) GetClusterScopedResources(ctx context.Context) []crv1alp
 			Name:       "strimzi-kafka-broker",
 			Resource:   "clusterroles",
 		},
-		
+
 		// ClusterRoleBindings
 		{
 			APIVersion: "v1",
@@ -222,7 +222,7 @@ func (kc *KafkaCluster) GetClusterScopedResources(ctx context.Context) []crv1alp
 			Name:       "strimzi-cluster-operator-kafka-client-delegation",
 			Resource:   "clusterrolebindings",
 		},
-		
+
 		// CRDs
 		{
 			APIVersion: "v1",
