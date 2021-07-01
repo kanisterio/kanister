d26610a96ad7b2c2594d3f365445f5adf2a253a9
Hugo Gayosso <hugo@kasten.io>
2021-07-01 09:50:51 -0400

    add swedensouth region

--- pkg/blockstorage/azure/azuredisk.go
+++ pkg/blockstorage/azure/azuredisk.go
@@ -731,6 +731,8 @@ func staticRegionToZones(region string) ([]string, error) {
 		return nil, nil
 	case "swedencentral":
 		return nil, nil
+	case "swedensouth":
+		return nil, nil
 	case "switzerlandnorth":
 		return nil, nil
 	case "switzerlandwest":
