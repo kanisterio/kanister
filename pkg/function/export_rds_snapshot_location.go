b2a104850677bde52eb1dea859477461c89155e2
A-kanksh-a <akankshakumari393@gmail.com>
2021-10-16 16:02:49 +0530

    Use latest tag for postgresToolsImage

--- pkg/function/export_rds_snapshot_location.go
+++ pkg/function/export_rds_snapshot_location.go
@@ -60,7 +60,7 @@ const (
 	BackupAction  RDSAction = "backup"
 	RestoreAction RDSAction = "restore"
 
-	postgresToolsImage = "ghcr.io/kanisterio/postgres-kanister-tools:0.68.0"
+	postgresToolsImage = "ghcr.io/kanisterio/postgres-kanister-tools:latest"
 )
 
 type exportRDSSnapshotToLocationFunc struct{}
