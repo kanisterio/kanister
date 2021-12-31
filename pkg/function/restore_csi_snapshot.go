dba3c4d4da30f9946172633dd57ab1b6fe1b8b3b
shlokchaudhari9 <shlokchaudhari9@gmail.com>
2021-12-31 12:00:17 +0530

    Updated the return obj for the function

--- pkg/function/restore_csi_snapshot.go
+++ pkg/function/restore_csi_snapshot.go
@@ -106,7 +106,7 @@ func (*restoreCSISnapshotFunc) Exec(ctx context.Context, tp param.TemplateParams
 	if err := restoreCSISnapshot(ctx, restoreArgs); err != nil {
 		return nil, err
 	}
-	return map[string]interface{}{}, nil
+	return nil, nil
 }
 
 func (*restoreCSISnapshotFunc) RequiredArgs() []string {
