9a7e889500e0e13b6dac0bc71aa4b5db57042864
chaitanya-baraskar <chaitanya.axe@gmail.com>
2022-01-12 18:56:55 +0530

    removing utils file

--- pkg/app/mssql.go
+++ pkg/app/mssql.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/ghodss/yaml"
 	"github.com/pkg/errors"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
@@ -19,7 +20,9 @@ import (
 )
 
 const (
-	mssqlWaitTimeout = 2 * time.Minute
+	mssqlWaitTimeout = 5 * time.Minute
+	dbUserName       = "sa"
+	dbPass           = "MyC0m9l&xP@ssw0rd"
 )
 
 type MssqlDB struct {
@@ -243,4 +246,103 @@ func (m MssqlDB) execCommand(ctx context.Context, command []string) (string, str
 		return "", "", errors.Wrapf(err, "Error getting pod and container name for app %s.", m.name)
 	}
 	return kube.Exec(m.cli, m.namespace, podName, containerName, command, nil)
+}
+
+func (m *MssqlDB) getDeploymentObj() (*appsv1.Deployment, error) {
+	deploymentManifest :=
+		`apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: mssql-deployment
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: mssql
+  template:
+    metadata:
+      labels:
+        app: mssql
+    spec:
+      terminationGracePeriodSeconds: 30
+      hostname: mssqlinst
+      securityContext:
+        fsGroup: 10001
+      containers:
+        - name: mssql
+          image: mcr.microsoft.com/mssql/server:2019-latest
+          ports:
+            - containerPort: 1433
+          env:
+            - name: MSSQL_PID
+              value: "Developer"
+            - name: ACCEPT_EULA
+              value: "Y"
+            - name: SA_PASSWORD
+              valueFrom:
+                secretKeyRef:
+                  name: mssql
+                  key: SA_PASSWORD
+          volumeMounts:
+            - name: mssqldb
+              mountPath: /var/opt/mssql
+      volumes:
+        - name: mssqldb
+          persistentVolumeClaim:
+            claimName: mssql-data`
+
+	var deployment *appsv1.Deployment
+	err := yaml.Unmarshal([]byte(deploymentManifest), &deployment)
+	return deployment, err
+}
+
+func (m *MssqlDB) getPVCObj() (*v1.PersistentVolumeClaim, error) {
+	pvcmaniFest :=
+		`kind: PersistentVolumeClaim
+apiVersion: v1
+metadata:
+  name: mssql-data
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 4Gi`
+
+	var pvc *v1.PersistentVolumeClaim
+	err := yaml.Unmarshal([]byte(pvcmaniFest), &pvc)
+	return pvc, err
+}
+
+func (m *MssqlDB) getServiceObj() (*v1.Service, error) {
+	serviceManifest :=
+		`apiVersion: v1
+kind: Service
+metadata:
+  name: mssql-deployment
+spec:
+  selector:
+    app: mssql
+  ports:
+    - protocol: TCP
+      port: 1433
+      targetPort: 1433
+  type: ClusterIP`
+
+	var service *v1.Service
+	err := yaml.Unmarshal([]byte(serviceManifest), &service)
+	return service, err
+
+}
+
+func (m MssqlDB) getSecretObj() *v1.Secret {
+	return &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: m.name,
+		},
+		Data: map[string][]byte{
+			"SA_PASSWORD": []byte(dbPass),
+		},
+		Type: "Opaque",
+	}
 }
