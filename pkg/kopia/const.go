cf4693ae2eee47fa24eeeb9a98efe1a996057f49
shlokchaudhari9 <shlokchaudhari9@gmail.com>
2022-08-01 15:35:21 +0530

    Remove unused const

--- pkg/kopia/const.go
+++ pkg/kopia/const.go
@@ -21,18 +21,6 @@ const (
 	// DefaultClientCacheDirectory is the directory where kopia content cache is created
 	DefaultClientCacheDirectory = "/tmp/kopia-cache"
 
-	// DefaultConfigDirectory is the directory which contains custom kopia repo config
-	DefaultConfigDirectory = "/tmp/kopia-repository"
-
-	// DefaultLogDirectory is the directory where kopia log file is created
-	DefaultLogDirectory = "/tmp/kopia-log"
-
-	// DefaultSparseRestore is the default option for whether to do a sparse restore
-	DefaultSparseRestore = false
-
-	// DefaultFSMountPath is the mount path for the file store PVC on Kopia API server
-	DefaultFSMountPath = "/mnt/data"
-
 	// DefaultDataStoreGeneralContentCacheSizeMB is the default content cache size for general command workloads
 	DefaultDataStoreGeneralContentCacheSizeMB = 0
 	// DataStoreGeneralContentCacheSizeMBVarName is the name of the environment variable that controls
