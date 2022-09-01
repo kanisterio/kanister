30552109e8c3aa9433a4112468c0f84cd071bbb4
Le Tran <le.tran@kasten.io>
2022-09-01 10:50:57 -0400

    Add authenticator for client secret credentials type
    Signed-off-by: Le Tran <le.tran@kasten.io>

--- pkg/blockstorage/azure/auth.go
+++ pkg/blockstorage/azure/auth.go
@@ -14,12 +14,11 @@ import (
 // to be available with azidentity: https://pkg.go.dev/github.com/Azure/azure-sdk-for-go/sdk/azidentity#readme-credential-types
 // to faciliate future migration, only the common ones between two SDKs are listed
 const (
-	CredTypeManagedIdentity   string = "cred-type-msi"
-	CredTypeClientSecret      string = "cred-type-client-secret"
-	CredTypeClientCertificate string = "cred-type-client-certificate"
+	CredTypeManagedIdentity string = "cred-type-msi"
+	CredTypeClientSecret    string = "cred-type-client-secret"
 )
 
-var availableCredsType = sets.NewString(CredTypeManagedIdentity)
+var availableCredsType = sets.NewString(CredTypeManagedIdentity, CredTypeClientSecret)
 
 type CredsValidity int
 
@@ -85,12 +84,55 @@ func (m *msiAuthenticator) authenticate(creds map[string]string) (CredsValidity,
 	return CredsPassedAuthentication, nil
 }
 
+type clientSecretAuthenticator struct{}
+
+func (c *clientSecretAuthenticator) authenticate(creds map[string]string) (CredsValidity, error) {
+	credConfig, err := getCredConfigForAuth(creds)
+	if err != nil {
+		return CredsFailedAuthentication, errors.Wrap(err, "Failed to get Client Secret config")
+	}
+	// create a service principal token
+	spt, err := credConfig.ServicePrincipalToken()
+	if err != nil {
+		return CredsFailedAuthentication, errors.Wrap(err, "Failed to create a service principal token")
+	}
+	// network call to check for token
+	err = spt.Refresh()
+	if err != nil {
+		return CredsFailedAuthentication, errors.Wrap(err, "Failed to refresh token")
+	}
+	// creds passed authentication
+	return CredsPassedAuthentication, nil
+}
+
 // return the authenticator based on credentials type
 func getAuthenticator(credType string) authenticator {
 	switch credType {
 	case CredTypeManagedIdentity:
 		return &msiAuthenticator{}
+	case CredTypeClientSecret:
+		return &clientSecretAuthenticator{}
 	default:
 		return nil
 	}
+}
+
+func getCredConfigForAuth(config map[string]string) (auth.ClientCredentialsConfig, error) {
+	tenantID, ok := config[blockstorage.AzureTenantID]
+	if !ok {
+		return auth.ClientCredentialsConfig{}, errors.New("Cannot get tenantID from config")
+	}
+
+	clientID, ok := config[blockstorage.AzureCientID]
+	if !ok {
+		return auth.ClientCredentialsConfig{}, errors.New("Cannot get clientID from config")
+	}
+
+	clientSecret, ok := config[blockstorage.AzureClentSecret]
+	if !ok {
+		return auth.ClientCredentialsConfig{}, errors.New("Cannot get clientSecret from config")
+	}
+
+	credConfig := auth.NewClientCredentialsConfig(clientID, clientSecret, tenantID)
+	return credConfig, nil
 }
