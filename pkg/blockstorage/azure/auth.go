2f361a70b8b77a1ae0cd283616b3d05bd26244e1
Le Tran <le.tran@kasten.io>
2022-09-01 11:49:00 -0400

    Add iota

--- pkg/blockstorage/azure/auth.go
+++ pkg/blockstorage/azure/auth.go
@@ -24,7 +24,7 @@ var availableCredsType = sets.NewString(CredTypeManagedIdentity, CredTypeClientS
 type CredsValidity int
 
 const (
-	CredValidityUnknown CredsValidity = 0
+	CredValidityUnknown CredsValidity = iota
 	CredsTypeNotSupported
 	CredsFailedAuthentication
 	CredsPassedAuthentication
