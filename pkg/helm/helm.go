5187415b46913baf8a5ed301d77dbef4dfb5f69e
Rajat Gupta <rajat.gupta@veeam.com>
2022-09-01 16:14:37 +0530

    Add a comment for wait argument
    Signed-off-by: Rajat Gupta <rajat.gupta@veeam.com>

--- pkg/helm/helm.go
+++ pkg/helm/helm.go
@@ -26,6 +26,7 @@ type Client interface {
 	// RemoveRepo removes helm repo
 	RemoveRepo(ctx context.Context, name string) error
 	// Install installs helm chart with given release name in the namespace
+	// wait argument enables/disables --wait flag in 'helm install' command
 	Install(ctx context.Context, chart, version, release, namespace string, values map[string]string, wait bool) error
 	// Uninstall deletes helm release from the given namespace
 	Uninstall(ctx context.Context, release, namespace string) error
