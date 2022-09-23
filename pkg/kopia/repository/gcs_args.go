36c04b7adc47dfed2aef5ca8b4fd8ac52067bd41
Ankit Jain <ankitjain.meone@gmail.com>
2022-09-23 17:42:16 +0530

    Add utility generating kopia CLI args for gcs

---
+++ pkg/kopia/repository/gcs_args.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	v1 "k8s.io/api/core/v1"
+
+	"github.com/kanisterio/kanister/pkg/consts"
+	"github.com/kanisterio/kanister/pkg/logsafe"
+)
+
+const (
+	gcsSubCommand       = "gcs"
+	credentialsFileFlag = "--credentials-file"
+	gcsBucketFlag       = "--bucket"
+	gcsPrefixFlag       = "--prefix"
+)
+
+func kopiaGCSArgs(locationSecret *v1.Secret, artifactPrefix string) logsafe.Cmd {
+	artifactPrefix = GenerateFullRepoPath(prefix(locationSecret), artifactPrefix)
+
+	args := logsafe.NewLoggable(gcsSubCommand)
+	args = args.AppendLoggableKV(gcsBucketFlag, bucketName(locationSecret))
+	args = args.AppendLoggableKV(credentialsFileFlag, consts.GoogleCloudCredsFilePath)
+	return args.AppendLoggableKV(gcsPrefixFlag, artifactPrefix)
+}
---
+++ pkg/kopia/repository/gcs_args_test.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"fmt"
+
+	"gopkg.in/check.v1"
+	v1 "k8s.io/api/core/v1"
+)
+
+func (s *RepositoryUtilsSuite) TestGCSArgsUtil(c *check.C) {
+	locSecret := &v1.Secret{
+		StringData: map[string]string{
+			prefixKey: "test-prefix",
+			bucketKey: "test-bucket",
+		},
+	}
+	artifactPrefix := "dir/sub-dir"
+	cmd := kopiaGCSArgs(locSecret, artifactPrefix)
+	c.Assert(cmd.String(), check.Equals, fmt.Sprint("gcs --bucket=",
+		locSecret.StringData[bucketKey],
+		" --credentials-file=/tmp/creds.txt",
+		" --prefix=", fmt.Sprintf("%s/%s/", locSecret.StringData[prefixKey], artifactPrefix),
+	))
+}
