04d7d9274f853f2ee906d11a0ba4a5f159a4efe2
shlokchaudhari9 <shlokchaudhari9@gmail.com>
2022-11-02 17:25:22 +0530

    Remove the clientset, listers and informers for RepositoryServer CR

--- pkg/client/listers/cr/v1alpha1/expansion_generated.go
+++ pkg/client/listers/cr/v1alpha1/expansion_generated.go
@@ -41,11 +41,3 @@ type ProfileListerExpansion interface{}
 // ProfileNamespaceListerExpansion allows custom methods to be added to
 // ProfileNamespaceLister.
 type ProfileNamespaceListerExpansion interface{}
-
-// RepositoryServerListerExpansion allows custom methods to be added to
-// RepositoryServerLister.
-type RepositoryServerListerExpansion interface{}
-
-// RepositoryServerNamespaceListerExpansion allows custom methods to be added to
-// RepositoryServerNamespaceLister.
-type RepositoryServerNamespaceListerExpansion interface{}
