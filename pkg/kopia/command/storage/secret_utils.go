94fe6d77346b7daf5181a9e29914d5698cf276e4
Ankit Jain <ankitjain.meone@gmail.com>
2022-11-03 15:27:47 +0530

    Add utilities to generate EnvVars for secret

--- pkg/kopia/command/storage/secret_utils.go
+++ pkg/kopia/command/storage/secret_utils.go
@@ -14,6 +14,18 @@
 
 package storage
 
+import (
+	"context"
+	"time"
+
+	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/pkg/errors"
+	v1 "k8s.io/api/core/v1"
+
+	"github.com/kanisterio/kanister/pkg/aws"
+	"github.com/kanisterio/kanister/pkg/secrets"
+)
+
 type LocType string
 
 const (
@@ -30,6 +42,11 @@ const (
 	LocTypeGCS       LocType = "gcs"
 	LocTypeAzure     LocType = "azure"
 	LocTypeFilestore LocType = "filestore"
+
+	// Azure location related environment variables
+	azureStorageAccountEnv = "AZURE_STORAGE_ACCOUNT"
+	azureStorageKeyEnv     = "AZURE_STORAGE_KEY"
+	azureStorageDomainEnv  = "AZURE_STORAGE_DOMAIN"
 )
 
 func getBucketNameFromMap(m map[string][]byte) string {
@@ -55,6 +72,94 @@ func checkSkipSSLVerifyFromMap(m map[string][]byte) bool {
 
 func locationType(m map[string][]byte) LocType {
 	return LocType(m[typeKey])
+}
+
+// GenerateEnvSpecFromCredentialSecret parses the secret and returns
+// list of EnvVar based on secret type
+func GenerateEnvSpecFromCredentialSecret(s *v1.Secret) ([]v1.EnvVar, error) {
+	if s == nil {
+		return nil, errors.New("Secret cannot be nil")
+	}
+	secType := string(s.Type)
+	switch secType {
+	case secrets.AWSSecretType:
+		return getEnvSpecForAWSCredentialSecret(s)
+	case secrets.AzureSecretType:
+		return getEnvSpecForAzureCredentialSecret(s)
+	}
+	// We only need to set the environment variables in cases where
+	// secret type is AWS or Azure.
+	return nil, nil
+}
+
+func getEnvSpecForAWSCredentialSecret(s *v1.Secret) ([]v1.EnvVar, error) {
+	var duration time.Duration
+	var err error
+	envVars := []v1.EnvVar{}
+	envVars = append(
+		envVars,
+		getEnvVarWithSecretRef(aws.AccessKeyID, s.Name, secrets.AWSAccessKeyID),
+		getEnvVarWithSecretRef(aws.SecretAccessKey, s.Name, secrets.AWSSecretAccessKey),
+	)
+	if val, ok := s.Data["assume_duration"]; ok {
+		duration, err = time.ParseDuration(string(val))
+		if err != nil {
+			return nil, errors.Wrap(err, "Failed to parse AWS Assume Role duration")
+		}
+	}
+	creds, err := secrets.ExtractAWSCredentials(context.Background(), s, duration)
+	if err != nil {
+		return nil, err
+	}
+	if creds.SessionToken != "" {
+		envVars = append(envVars, getEnvVarWithSecretRef(aws.SessionToken, s.Name, secrets.AWSSessionToken))
+	}
+	return envVars, nil
+}
+
+func getEnvSpecForAzureCredentialSecret(s *v1.Secret) ([]v1.EnvVar, error) {
+	envVars := []v1.EnvVar{}
+	envVars = append(
+		envVars,
+		getEnvVarWithSecretRef(azureStorageAccountEnv, s.Name, secrets.AzureStorageAccountID),
+		getEnvVarWithSecretRef(azureStorageKeyEnv, s.Name, secrets.AzureStorageAccountKey),
+	)
+	azureSecret, err := secrets.ExtractAzureCredentials(s)
+	if err != nil {
+		return nil, err
+	}
+	storageEnv := azureSecret.EnvironmentName
+	if storageEnv != "" {
+		env, err := azure.EnvironmentFromName(storageEnv)
+		if err != nil {
+			return nil, errors.Wrapf(err, "Failed to get azure environment from name: %s", storageEnv)
+		}
+		blobDomain := "blob." + env.StorageEndpointSuffix
+		// TODO : Check how we can set this env to use value from secret
+		envVars = append(envVars, getEnvVar(azureStorageDomainEnv, blobDomain))
+	}
+	return envVars, nil
+}
+
+func getEnvVarWithSecretRef(varName, secretName, secretKey string) v1.EnvVar {
+	return v1.EnvVar{
+		Name: varName,
+		ValueFrom: &v1.EnvVarSource{
+			SecretKeyRef: &v1.SecretKeySelector{
+				Key: secretKey,
+				LocalObjectReference: v1.LocalObjectReference{
+					Name: secretName,
+				},
+			},
+		},
+	}
+}
+
+func getEnvVar(varName, value string) v1.EnvVar {
+	return v1.EnvVar{
+		Name:  varName,
+		Value: value,
+	}
 }
 
 // GetMapForLocationValues return a map with valid keys
