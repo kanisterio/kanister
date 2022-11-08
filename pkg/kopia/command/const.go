e9dc74e98209631f5a5cf57b2aa3b2e1fcf5bf6c
Rajat Gupta <rajat.gupta@veeam.com>
2022-11-08 12:41:03 +0530

    Add Constants

--- pkg/kopia/command/const.go
+++ pkg/kopia/command/const.go
@@ -72,4 +72,16 @@ const (
 	tlsGenerateCertFlag       = "--tls-generate-cert"
 	tlsKeyFilePath            = "--tls-key-file"
 	userPasswordFlag          = "--user-password"
+
+	// DefaultCacheDirectory is the directory where kopia content cache is created
+	DefaultCacheDirectory = "/tmp/kopia-cache"
+
+	// DefaultConfigFilePath is the file which contains kopia repo config
+	DefaultConfigFilePath = "/tmp/kopia-repository.config"
+
+	// DefaultConfigDirectory is the directory which contains custom kopia repo config
+	DefaultConfigDirectory = "/tmp/kopia-repository"
+
+	// DefaultLogDirectory is the directory where kopia log file is created
+	DefaultLogDirectory = "/tmp/kopia-log"
 )
