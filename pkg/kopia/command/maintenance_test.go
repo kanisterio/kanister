b3dbe371529787f0174492a4de6d4dca8355d63c
Rajat Gupta <rajat.gupta@veeam.com>
2022-11-08 13:45:09 +0530

    Add Unit Tests for maintenance.go

---
+++ pkg/kopia/command/maintenance_test.go
@@ -0,0 +1,79 @@
+// Copyright 2022 The Kanister Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package command
+
+import (
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+)
+
+func TestMaintenanceCommands(t *testing.T) {
+	c := qt.New(t)
+
+	commandArgs := &CommandArgs{
+		RepoPassword:   "encr-key",
+		ConfigFilePath: "path/kopia.config",
+		LogDirectory:   "cache/log",
+	}
+
+	for _, tc := range []struct {
+		f           func() []string
+		expectedLog string
+	}{
+		{
+			f: func() []string {
+				args := MaintenanceInfoCommandArgs{
+					CommandArgs:   commandArgs,
+					GetJsonOutput: false,
+				}
+				return MaintenanceInfo(args)
+			},
+			expectedLog: "kopia --log-level=error --config-file=path/kopia.config --log-dir=cache/log --password=<****> maintenance info",
+		},
+		{
+			f: func() []string {
+				args := MaintenanceInfoCommandArgs{
+					CommandArgs:   commandArgs,
+					GetJsonOutput: true,
+				}
+				return MaintenanceInfo(args)
+			},
+			expectedLog: "kopia --log-level=error --config-file=path/kopia.config --log-dir=cache/log --password=<****> maintenance info --json",
+		},
+		{
+			f: func() []string {
+				args := MaintenanceSetOwnerCommandArgs{
+					CommandArgs: commandArgs,
+					CustomOwner: "username@hostname",
+				}
+				return MaintenanceSetOwner(args)
+			},
+			expectedLog: "kopia --log-level=error --config-file=path/kopia.config --log-dir=cache/log --password=<****> maintenance set --owner=username@hostname",
+		},
+		{
+			f: func() []string {
+				args := MaintenanceRunCommandArgs{
+					CommandArgs: commandArgs,
+				}
+				return MaintenanceRunCommand(args)
+			},
+			expectedLog: "kopia --log-level=error --config-file=path/kopia.config --log-dir=cache/log --password=<****> maintenance run",
+		},
+	} {
+		cmd := tc.f()
+		c.Check(cmd, qt.Equals, tc.expectedLog)
+	}
+}
