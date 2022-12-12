e5cb102bb50470836e4cd5ffcb102ff31d8c9b05
kale-amruta <amrutakale24.1991@gmail.com>
2022-12-12 13:24:14 +0530

    remove network policy lifecyccle from repository controller

--- pkg/controllers/repositoryserver/handler.go
+++ pkg/controllers/repositoryserver/handler.go
@@ -25,10 +25,8 @@ import (
 	"github.com/jpillora/backoff"
 	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
-	networkingv1 "k8s.io/api/networking/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -53,9 +51,6 @@ type RepoServerHandler struct {
 func (h *RepoServerHandler) CreateOrUpdateOwnedResources() error {
 	svc, err := h.reconcileService()
 	if err != nil {
-		return err
-	}
-	if err := h.reconcileNetworkPolicy(svc); err != nil {
 		return err
 	}
 	if err = h.getSecretsFromCR(); err != nil {
@@ -94,9 +89,8 @@ func (h *RepoServerHandler) reconcileService() (*corev1.Service, error) {
 	}
 	h.Logger.Info("Update serviceName in RepositoryServer /status")
 	serverInfo := crv1alpha1.ServerInfo{
-		PodName:           h.RepositoryServer.Status.ServerInfo.PodName,
-		ServiceName:       svc.Name,
-		NetworkPolicyName: h.RepositoryServer.Status.ServerInfo.NetworkPolicyName,
+		PodName:     h.RepositoryServer.Status.ServerInfo.PodName,
+		ServiceName: svc.Name,
 	}
 	if err := h.updateServerInfoInCRStatus(serverInfo); err != nil {
 		return nil, errors.Wrap(err, "Failed to update ServiceName in RepositoryServer /status")
@@ -156,104 +150,6 @@ func (h *RepoServerHandler) createService(repoServerNamespace string) (*corev1.S
 	return &svc, nil
 }
 
-func (h *RepoServerHandler) reconcileNetworkPolicy(svc *corev1.Service) error {
-	repoServerNamespace := h.RepositoryServer.Namespace
-	networkPolicyName := h.RepositoryServer.Status.ServerInfo.NetworkPolicyName
-	np := &networkingv1.NetworkPolicy{}
-	h.Logger.Info("Check if NetworkPolicy resource exists. If exists, reconcile with CR spec")
-	err := h.Reconciler.Get(h.Ctx, types.NamespacedName{Name: networkPolicyName, Namespace: repoServerNamespace}, np)
-	if err == nil {
-		return h.updateNetworkPolicy(np, svc)
-	}
-	if !apierrors.IsNotFound(err) {
-		return err
-	}
-	h.Logger.Info("NetworkPolicy resource not found. Creating new networkPolicy")
-	np, err = h.createNetworkPolicy(repoServerNamespace, svc)
-	if err != nil {
-		return err
-	}
-	h.Logger.Info("Update networkPolicyName in RepositoryServer /status")
-	serverInfo := crv1alpha1.ServerInfo{
-		PodName:           h.RepositoryServer.Status.ServerInfo.PodName,
-		ServiceName:       h.RepositoryServer.Status.ServerInfo.ServiceName,
-		NetworkPolicyName: np.Name,
-	}
-	if err := h.updateServerInfoInCRStatus(serverInfo); err != nil {
-		return errors.Wrap(err, "Failed to update networkPolicyName in RepositoryServer /status")
-	}
-	return nil
-}
-
-func (h *RepoServerHandler) updateNetworkPolicy(np *networkingv1.NetworkPolicy, svc *corev1.Service) error {
-	np, err := h.updateServiceNameInNetworkPolicyLabels(np, svc)
-	if err != nil {
-		return err
-	}
-	np = h.updateIngressRuleInNetworkPolicy(np)
-	if err := h.Reconciler.Update(h.Ctx, np); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (h *RepoServerHandler) updateServiceNameInNetworkPolicyLabels(
-	np *networkingv1.NetworkPolicy,
-	svc *corev1.Service) (*networkingv1.NetworkPolicy, error) {
-	h.Logger.Info("Check if current svcName matches in NP labels and podSelectors")
-	if np.ObjectMeta.Labels[repoServerServiceNameKey] == svc.Name &&
-		np.Spec.PodSelector.MatchLabels[repoServerServiceNameKey] == svc.Name {
-		h.Logger.Info("Skipping NP Label update. Current svcName matches with NP labels and podSelectors")
-		return np, nil
-	}
-	h.Logger.Info("Current svcName does not match in NP labels. Update NP with new svcName")
-	currentLabel := map[string]string{repoServerServiceNameKey: svc.Name}
-	np.ObjectMeta.Labels = currentLabel
-	np.Spec.PodSelector.MatchLabels = currentLabel
-	return np, nil
-}
-
-func (h *RepoServerHandler) updateIngressRuleInNetworkPolicy(
-	np *networkingv1.NetworkPolicy) *networkingv1.NetworkPolicy {
-	protocolTCP := corev1.ProtocolTCP
-	port := intstr.FromInt(repoServerServicePort)
-	ingressRule := []networkingv1.NetworkPolicyIngressRule{
-		{
-			From: []networkingv1.NetworkPolicyPeer{
-				{
-					PodSelector:       h.RepositoryServer.Spec.Server.NetworkPolicyIngressRule.PodSelector,
-					NamespaceSelector: h.RepositoryServer.Spec.Server.NetworkPolicyIngressRule.NamespaceSelector,
-				},
-			},
-			Ports: []networkingv1.NetworkPolicyPort{
-				{
-					Protocol: &protocolTCP,
-					Port:     &port,
-				},
-			},
-		},
-	}
-	np.Spec.Ingress = ingressRule
-	return np
-}
-
-func (h *RepoServerHandler) createNetworkPolicy(
-	repoServerNamespace string,
-	svc *corev1.Service) (*networkingv1.NetworkPolicy, error) {
-	podSelector := h.RepositoryServer.Spec.Server.NetworkPolicyIngressRule.PodSelector
-	namespaceSelector := h.RepositoryServer.Spec.Server.NetworkPolicyIngressRule.NamespaceSelector
-	np := getRepoServerNetworkPolicy(repoServerNamespace, svc, podSelector, namespaceSelector)
-	h.Logger.Info("Set controller reference on NetworkPolicy to allow reconciliation using this controller")
-	if err := controllerutil.SetControllerReference(h.RepositoryServer, np, h.Reconciler.Scheme); err != nil {
-		return nil, err
-	}
-	err := h.Reconciler.Create(h.Ctx, np)
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to create RepositoryServer networkPolicy")
-	}
-	return np, nil
-}
-
 func (h *RepoServerHandler) reconcilePod(svc *corev1.Service) (*corev1.Pod, error) {
 	repoServerNamespace := h.RepositoryServer.Namespace
 	podName := h.RepositoryServer.Status.ServerInfo.PodName
@@ -273,9 +169,8 @@ func (h *RepoServerHandler) reconcilePod(svc *corev1.Service) (*corev1.Pod, erro
 	}
 	h.Logger.Info("Update podName in RepositoryServer /status")
 	serverInfo := crv1alpha1.ServerInfo{
-		PodName:           pod.Name,
-		ServiceName:       h.RepositoryServer.Status.ServerInfo.ServiceName,
-		NetworkPolicyName: h.RepositoryServer.Status.ServerInfo.NetworkPolicyName,
+		PodName:     pod.Name,
+		ServiceName: h.RepositoryServer.Status.ServerInfo.ServiceName,
 	}
 	if err := h.updateServerInfoInCRStatus(serverInfo); err != nil {
 		return nil, errors.Wrap(err, "Failed to update podName in RepositoryServer /status")
@@ -333,7 +228,7 @@ func (h *RepoServerHandler) setCredDataFromSecretInPod(podOptions *kube.PodOptio
 	h.Logger.Info("Setting credentials data from secret as either env variables or files in pod")
 	namespace := h.RepositoryServer.Namespace
 	storageCredSecret := h.RepositoryServerSecrets.storageCredentials
-	envVars, err := storage.GenerateEnvSpecFromCredentialSecret(&storageCredSecret)
+	envVars, err := storage.GenerateEnvSpecFromCredentialSecret(&storageCredSecret, time.Duration(time.Now().Second()))
 	if err != nil {
 		return nil, err
 	}
