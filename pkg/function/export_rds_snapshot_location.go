6e56a5bf7d00d86b14654c2af675804f44b1de4c
Kasten Production <infra@kasten.io>
2023-01-12 19:24:34 +0000

    Kanister docs update to version 0.86.0

--- pkg/function/export_rds_snapshot_location.go
+++ pkg/function/export_rds_snapshot_location.go
@@ -60,7 +60,7 @@ const (
 	BackupAction  RDSAction = "backup"
 	RestoreAction RDSAction = "restore"
 
-	postgresToolsImage = "ghcr.io/kanisterio/postgres-kanister-tools:0.85.0"
+	postgresToolsImage = "ghcr.io/kanisterio/postgres-kanister-tools:0.86.0"
 )
 
 type exportRDSSnapshotToLocationFunc struct{}
