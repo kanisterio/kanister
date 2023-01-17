5eaee0400eb643c43cd5c38538ae94c0c9da2dd6
KastenMike <93532247+KastenMike@users.noreply.github.com>
2023-01-17 09:43:59 +0100

    add ingMgr Update func

--- pkg/kube/ingress/ingressmgr.go
+++ pkg/kube/ingress/ingressmgr.go
@@ -41,13 +41,15 @@ const (
 type Manager interface {
 	// List can be used to list all the ingress resources from `ns` namespace
 	List(ctx context.Context, ns string) (runtime.Object, error)
-	// Get can be used to to get ingress resource with name `name` in `ns` namespace
+	// Get can be used to get ingress resource with name `name` in `ns` namespace
 	Get(ctx context.Context, ns, name string) (runtime.Object, error)
 	// IngressPath can be used to get the backend path that is specified in the
 	// ingress resource in `ns` namespace and name `releaseName-ingress`
 	IngressPath(ctx context.Context, ns, releaseName string) (string, error)
 	// Create accepts an ingress in as runtime.Object and creates on the cluster
 	Create(ctx context.Context, ingress *unstructured.Unstructured, opts metav1.CreateOptions) (runtime.Object, error)
+	// Update accepts an ingress in as runtime.Object and updates it on the cluster
+	Update(ctx context.Context, ingress *unstructured.Unstructured, opts metav1.UpdateOptions) (runtime.Object, error)
 }
 
 // NewManager can be used to get the Manager based on the APIVersion of the ingress resources on the cluster
