2a3af329c0fd7c3e49535b33cc3e5bd05b5fb13f
Eugen Sumin <eugen.sumin@kasten.io>
2023-02-10 11:57:02 +0100

    Implement PodController interface

--- pkg/kube/pod_controller.go
+++ pkg/kube/pod_controller.go
@@ -18,7 +18,22 @@ import (
 	"context"
 	"time"
 
+	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/kanisterio/kanister/pkg/field"
+	"github.com/kanisterio/kanister/pkg/log"
+)
+
+var (
+	ErrPodControllerNotInitialized = errors.New("pod has not been initialized")
+	ErrPodControllerAlreadyStarted = errors.New("pod has already been started")
+	ErrPodControllerNotReady       = errors.New("pod not ready yet")
+	ErrPodControllerNotStarted     = errors.New("pod not yet started")
+	PodControllerDefaultStopTime   = 30 * time.Second
+	PodControllerInfiniteStopTime  = 0 * time.Second
 )
 
 // PodController specifies interface needed for starting, stopping pod and operations with it
@@ -30,4 +45,140 @@ type PodController interface {
 	StopPod(ctx context.Context) error
 	GetCommandExecutor() (PodCommandExecutor, error)
 	GetFileWriter() (PodFileWriter, error)
+}
+
+// podControllerProcessor aids in unit testing
+type podControllerProcessor interface {
+	createPod(ctx context.Context, cli kubernetes.Interface, options *PodOptions) (*corev1.Pod, error)
+	waitForPodReady(ctx context.Context, podName string) error
+	deletePod(ctx context.Context, namespace string, podName string, opts metav1.DeleteOptions) error
+}
+
+// podController specifies Kubernetes Client and PodOptions needed for creating Pod
+type podController struct {
+	cli        kubernetes.Interface
+	podOptions *PodOptions
+
+	pod         *corev1.Pod
+	podReady    bool
+	podName     string
+	stopTimeout time.Duration
+
+	pcp podControllerProcessor
+}
+
+type PodControllerOption func(p *podController)
+
+// WithPodControllerProcessor provides mechanism for passing fake podControllerProcessor for testing purposes.
+func WithPodControllerProcessor(processor podControllerProcessor) PodControllerOption {
+	return func(p *podController) {
+		p.pcp = processor
+	}
+}
+
+// NewPodController returns a new PodController given Kubernetes Client and PodOptions
+func NewPodController(cli kubernetes.Interface, options *PodOptions, opts ...PodControllerOption) PodController {
+	r := &podController{
+		cli:        cli,
+		podOptions: options,
+	}
+
+	r.pcp = r
+
+	for _, opt := range opts {
+		opt(r)
+	}
+
+	return r
+}
+
+func (p *podController) PodName() string {
+	return p.podName
+}
+
+func (p *podController) Pod() *corev1.Pod {
+	return p.pod
+}
+
+// StartPod creates pod and in case of success, it stores pod name for further use.
+// stopTimeout is also stored and will be used when StopPod will be called
+func (p *podController) StartPod(ctx context.Context, stopTimeout time.Duration) error {
+	if p.podName != "" {
+		return errors.Wrap(ErrPodControllerAlreadyStarted, "Failed to create pod")
+	}
+
+	if p.cli == nil || p.podOptions == nil {
+		return errors.Wrap(ErrPodControllerNotInitialized, "Failed to create pod")
+	}
+
+	pod, err := p.pcp.createPod(ctx, p.cli, p.podOptions)
+	if err != nil {
+		log.WithError(err).Print("Failed to create pod", field.M{"PodName": p.podOptions.Name, "Namespace": p.podOptions.Namespace})
+		return errors.Wrap(err, "Failed to create pod")
+	}
+
+	p.pod = pod
+	p.podName = pod.Name
+	p.stopTimeout = stopTimeout
+
+	return nil
+}
+
+// WaitForPod waits for POD readiness.
+func (p *podController) WaitForPodReady(ctx context.Context) error {
+	if p.podName == "" {
+		return errors.Wrap(ErrPodControllerNotStarted, "Pod failed to become ready in time")
+	}
+
+	if err := p.pcp.waitForPodReady(ctx, p.podName); err != nil {
+		log.WithError(err).Print("Pod failed to become ready in time", field.M{"PodName": p.podName, "Namespace": p.podOptions.Namespace})
+		_ = p.StopPod(ctx) // best-effort
+		return errors.Wrap(err, "Pod failed to become ready in time")
+	}
+
+	p.podReady = true
+
+	return nil
+}
+
+// StopPod stops the pod which was previously started, otherwise it will return ErrPodControllerNotStarted error.
+// stopTimeout passed to Start will be used
+func (p *podController) StopPod(ctx context.Context) error {
+	if p.podName == "" {
+		return ErrPodControllerNotStarted
+	}
+
+	if p.stopTimeout != PodControllerInfiniteStopTime {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, p.stopTimeout)
+		defer cancel()
+	}
+
+	gracePeriodSeconds := int64(0) // force immediate deletion
+
+	if err := p.pcp.deletePod(ctx, p.podOptions.Namespace, p.podName, metav1.DeleteOptions{GracePeriodSeconds: &gracePeriodSeconds}); err != nil {
+		log.WithError(err).Print("Failed to delete pod", field.M{"PodName": p.podName, "Namespace": p.podOptions.Namespace})
+		return err
+	}
+
+	p.podReady = false
+	p.podName = ""
+	p.pod = nil
+
+	return nil
+}
+
+// This is wrapped for unit testing.
+func (p *podController) createPod(ctx context.Context, cli kubernetes.Interface, options *PodOptions) (*corev1.Pod, error) {
+	return CreatePod(ctx, cli, options)
+}
+
+// This is wrapped for unit testing.
+func (p *podController) waitForPodReady(ctx context.Context, podName string) error {
+	return WaitForPodReady(ctx, p.cli, p.podOptions.Namespace, podName)
+}
+
+// This is wrapped for unit testing.
+func (p *podController) deletePod(ctx context.Context, namespace string, podName string, opts metav1.DeleteOptions) error {
+	return p.cli.CoreV1().Pods(namespace).Delete(ctx, podName, opts)
 }
---
+++ pkg/kube/pod_controller_test.go
@@ -0,0 +1,364 @@
+// Copyright 2019 The Kanister Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kube
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/pkg/errors"
+	. "gopkg.in/check.v1"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+type PodControllerTestSuite struct{}
+
+var _ = Suite(&PodControllerTestSuite{})
+
+const (
+	podControllerNS      = "pod-runner-test"
+	podControllerPodName = "test-pod"
+)
+
+func (s *PodControllerTestSuite) SetUpSuite(c *C) {
+	os.Setenv("POD_NAMESPACE", podControllerNS)
+}
+
+type fakePodControllerProcessor struct {
+	inWaitForPodReadyPodName string
+	waitForPodReadyErr       error
+
+	inDeletePodNamespace string
+	inDeletePodPodName   string
+	inDeletePodOptions   metav1.DeleteOptions
+	deletePodErr         error
+
+	inCreatePodCli     kubernetes.Interface
+	inCreatePodOptions *PodOptions
+	createPodRet       *v1.Pod
+	createPodErr       error
+}
+
+func (fprp *fakePodControllerProcessor) createPod(_ context.Context, cli kubernetes.Interface, options *PodOptions) (*v1.Pod, error) {
+	fprp.inCreatePodCli = cli
+	fprp.inCreatePodOptions = options
+	return fprp.createPodRet, fprp.createPodErr
+}
+
+func (fprp *fakePodControllerProcessor) waitForPodReady(_ context.Context, podName string) error {
+	fprp.inWaitForPodReadyPodName = podName
+	return fprp.waitForPodReadyErr
+}
+
+func (fprp *fakePodControllerProcessor) deletePod(_ context.Context, namespace string, podName string, opts metav1.DeleteOptions) error {
+	fprp.inDeletePodNamespace = namespace
+	fprp.inDeletePodPodName = podName
+	fprp.inDeletePodOptions = opts
+
+	return fprp.deletePodErr
+}
+
+func (s *PodControllerTestSuite) TestPodControllerStartPod(c *C) {
+	ctx := context.Background()
+	cli := fake.NewSimpleClientset()
+
+	simulatedError := errors.New("SimulatedError")
+
+	cases := map[string]func(prp *fakePodControllerProcessor, pr PodController){
+		"Pod creation failure": func(pcp *fakePodControllerProcessor, pc PodController) {
+			pcp.createPodErr = simulatedError
+			err := pc.StartPod(ctx, 30*time.Second)
+			c.Assert(err, Not(IsNil))
+			c.Assert(errors.Is(err, simulatedError), Equals, true)
+			c.Assert(pcp.inCreatePodCli, Equals, cli)
+			c.Assert(pcp.inCreatePodOptions, DeepEquals, &PodOptions{
+				Namespace: podControllerNS,
+				Name:      podControllerPodName,
+			})
+		},
+		"Pod successfully started": func(prp *fakePodControllerProcessor, pr PodController) {
+			prp.createPodRet = &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: podControllerPodName,
+				},
+			}
+			err := pr.StartPod(ctx, 30*time.Second)
+			c.Assert(err, IsNil)
+			c.Assert(pr.PodName(), Equals, podControllerPodName)
+		},
+		"Pod already created": func(prp *fakePodControllerProcessor, pr PodController) {
+			prp.createPodRet = &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: podControllerPodName,
+				},
+			}
+
+			err := pr.StartPod(ctx, 30*time.Second)
+			c.Assert(err, IsNil)
+			c.Assert(prp.inCreatePodCli, Equals, cli)
+
+			prp.inCreatePodCli = nil
+			prp.inCreatePodOptions = nil
+			prp.createPodRet = nil
+			prp.createPodErr = errors.New("CreatePod should not be invoked")
+
+			err = pr.StartPod(ctx, 30*time.Second)
+			c.Assert(err, Not(IsNil))
+			c.Assert(errors.Is(err, ErrPodControllerAlreadyStarted), Equals, true)
+			c.Assert(prp.inCreatePodCli, IsNil)
+			c.Assert(prp.inCreatePodOptions, IsNil)
+		},
+	}
+
+	for l, tc := range cases {
+		c.Log(l)
+
+		pcp := &fakePodControllerProcessor{}
+
+		pc := NewPodController(cli, &PodOptions{
+			Namespace: podControllerNS,
+			Name:      podControllerPodName,
+		}, WithPodControllerProcessor(pcp))
+
+		tc(pcp, pc)
+	}
+}
+
+func (s *PodControllerTestSuite) TestPodControllerWaitPod(c *C) {
+	ctx := context.Background()
+	cli := fake.NewSimpleClientset()
+
+	simulatedError := errors.New("SimulatedError")
+
+	cases := map[string]func(pcp *fakePodControllerProcessor, pc PodController){
+		"Waiting failed because pod not started yet": func(pcp *fakePodControllerProcessor, pc PodController) {
+			err := pc.WaitForPodReady(ctx)
+			c.Assert(err, Not(IsNil))
+			c.Assert(errors.Is(err, ErrPodControllerNotStarted), Equals, true)
+			c.Assert(pcp.inCreatePodOptions, IsNil)
+			c.Assert(pcp.inCreatePodCli, IsNil)
+		},
+		"Waiting failed due to timeout": func(pcp *fakePodControllerProcessor, pc PodController) {
+			pcp.createPodRet = &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: podControllerPodName,
+				},
+			}
+			err := pc.StartPod(ctx, 30*time.Second)
+			c.Assert(err, IsNil)
+
+			pcp.waitForPodReadyErr = simulatedError
+			err = pc.WaitForPodReady(ctx)
+			c.Assert(err, Not(IsNil))
+			c.Assert(pcp.inWaitForPodReadyPodName, Equals, podControllerPodName)
+			c.Assert(errors.Is(err, pcp.waitForPodReadyErr), Equals, true)
+			c.Assert(err.Error(), Equals, fmt.Sprintf("Pod failed to become ready in time: %s", simulatedError.Error()))
+			// Check that POD deletion was also invoked with expected arguments
+			c.Assert(pcp.inDeletePodPodName, Equals, podControllerPodName)
+			c.Assert(pcp.inDeletePodNamespace, Equals, podControllerNS)
+			gracePeriodSeconds := int64(0)
+			c.Assert(pcp.inDeletePodOptions, DeepEquals, metav1.DeleteOptions{GracePeriodSeconds: &gracePeriodSeconds})
+		},
+		"Waiting failure returned even if pod deletion failed too": func(pcp *fakePodControllerProcessor, pc PodController) { // TODO(e-sumin): Both errors should be returned
+			pcp.createPodRet = &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: podControllerPodName,
+				},
+			}
+			err := pc.StartPod(ctx, 30*time.Second)
+			c.Assert(err, IsNil)
+
+			pcp.waitForPodReadyErr = simulatedError
+			pcp.deletePodErr = errors.New("This error should not be returned")
+			err = pc.WaitForPodReady(ctx)
+			c.Assert(err, Not(IsNil))
+			c.Assert(pcp.inWaitForPodReadyPodName, Equals, podControllerPodName)
+			c.Assert(errors.Is(err, pcp.waitForPodReadyErr), Equals, true)
+			c.Assert(err.Error(), Equals, fmt.Sprintf("Pod failed to become ready in time: %s", simulatedError.Error()))
+			// Check that POD deletion was invoked
+			c.Assert(pcp.inDeletePodPodName, Equals, podControllerPodName)
+			c.Assert(pcp.inDeletePodNamespace, Equals, podControllerNS)
+			gracePeriodSeconds := int64(0)
+			c.Assert(pcp.inDeletePodOptions, DeepEquals, metav1.DeleteOptions{GracePeriodSeconds: &gracePeriodSeconds})
+		},
+		"Waiting succeeded": func(pcp *fakePodControllerProcessor, pc PodController) {
+			pcp.createPodRet = &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: podControllerPodName,
+				},
+			}
+			err := pc.StartPod(ctx, 30*time.Second)
+			c.Assert(err, IsNil)
+			err = pc.WaitForPodReady(ctx)
+			c.Assert(err, IsNil)
+		},
+	}
+
+	for l, tc := range cases {
+		c.Log(l)
+
+		pcp := &fakePodControllerProcessor{}
+
+		pc := NewPodController(cli, &PodOptions{
+			Namespace: podControllerNS,
+			Name:      podControllerPodName,
+		}, WithPodControllerProcessor(pcp))
+
+		tc(pcp, pc)
+	}
+}
+
+func (s *PodControllerTestSuite) TestPodControllerStopPod(c *C) {
+	ctx := context.Background()
+	cli := fake.NewSimpleClientset()
+
+	untouchedStr := "DEADBEEF"
+	simulatedError := errors.New("SimulatedError")
+
+	cases := map[string]func(pcp *fakePodControllerProcessor, pc PodController){
+		"Pod not started yet": func(pcp *fakePodControllerProcessor, pc PodController) {
+			err := pc.StopPod(ctx)
+			c.Assert(err, Not(IsNil))
+			c.Assert(errors.Is(err, ErrPodControllerNotStarted), Equals, true)
+			c.Assert(pcp.inDeletePodPodName, Equals, untouchedStr)
+			c.Assert(pcp.inDeletePodNamespace, Equals, untouchedStr)
+		},
+		"Pod deletion error": func(pcp *fakePodControllerProcessor, pc PodController) {
+			pcp.createPodRet = &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: podControllerPodName,
+				},
+			}
+			err := pc.StartPod(ctx, 30*time.Second)
+			c.Assert(err, IsNil)
+
+			pcp.deletePodErr = simulatedError
+			err = pc.StopPod(ctx)
+			c.Assert(err, Not(IsNil))
+			c.Assert(errors.Is(err, simulatedError), Equals, true)
+		},
+		"Pod successfully deleted": func(pcp *fakePodControllerProcessor, pc PodController) {
+			pcp.createPodRet = &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: podControllerPodName,
+				},
+			}
+			err := pc.StartPod(ctx, 30*time.Second)
+			c.Assert(err, IsNil)
+
+			err = pc.StopPod(ctx)
+			c.Assert(err, IsNil)
+			c.Assert(pcp.inDeletePodPodName, Equals, podControllerPodName)
+			c.Assert(pcp.inDeletePodNamespace, Equals, podControllerNS)
+			gracePeriodSeconds := int64(0)
+			c.Assert(pcp.inDeletePodOptions, DeepEquals, metav1.DeleteOptions{GracePeriodSeconds: &gracePeriodSeconds})
+		},
+	}
+
+	for l, tc := range cases {
+		c.Log(l)
+
+		pcp := &fakePodControllerProcessor{
+			inDeletePodNamespace: untouchedStr,
+			inDeletePodPodName:   untouchedStr,
+			inDeletePodOptions:   metav1.DeleteOptions{},
+		}
+
+		pc := NewPodController(cli, &PodOptions{
+			Namespace: podControllerNS,
+			Name:      podName,
+		}, WithPodControllerProcessor(pcp))
+
+		tc(pcp, pc)
+	}
+}
+
+func (s *PodControllerTestSuite) TestPodControllerGetCommandExecutorAndFileWriter(c *C) {
+	ctx := context.Background()
+	cli := fake.NewSimpleClientset()
+
+	cases := map[string]func(pcp *fakePodControllerProcessor, pc PodController){
+		"Pod not started yet": func(_ *fakePodControllerProcessor, pc PodController) {
+			pce, err := pc.GetCommandExecutor()
+			c.Assert(pce, IsNil)
+			c.Assert(err, Not(IsNil))
+			c.Assert(errors.Is(err, ErrPodControllerNotStarted), Equals, true)
+
+			pfw, err := pc.GetFileWriter()
+			c.Assert(pfw, IsNil)
+			c.Assert(err, Not(IsNil))
+			c.Assert(errors.Is(err, ErrPodControllerNotStarted), Equals, true)
+		},
+		"Pod not ready yet": func(pcp *fakePodControllerProcessor, pc PodController) {
+			pcp.createPodRet = &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: podControllerPodName,
+				},
+			}
+			err := pc.StartPod(ctx, 30*time.Second)
+			c.Assert(err, IsNil)
+
+			pce, err := pc.GetCommandExecutor()
+			c.Assert(pce, IsNil)
+			c.Assert(err, Not(IsNil))
+			c.Assert(errors.Is(err, ErrPodControllerNotReady), Equals, true)
+
+			pfw, err := pc.GetFileWriter()
+			c.Assert(pfw, IsNil)
+			c.Assert(err, Not(IsNil))
+			c.Assert(errors.Is(err, ErrPodControllerNotReady), Equals, true)
+		},
+		"CommandExecutor successfully returned": func(pcp *fakePodControllerProcessor, pc PodController) {
+			pcp.createPodRet = &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: podControllerPodName,
+				},
+			}
+			err := pc.StartPod(ctx, 30*time.Second)
+			c.Assert(err, IsNil)
+
+			err = pc.WaitForPodReady(ctx)
+			c.Assert(err, IsNil)
+
+			var epce PodCommandExecutor
+			pce, err := pc.GetCommandExecutor()
+			c.Assert(err, IsNil)
+			c.Assert(pce, Implements, &epce)
+
+			var epfw PodFileWriter
+			pfw, err := pc.GetFileWriter()
+			c.Assert(err, IsNil)
+			c.Assert(pfw, Implements, &epfw)
+		},
+	}
+
+	for l, tc := range cases {
+		c.Log(l)
+
+		pcp := &fakePodControllerProcessor{}
+
+		pc := NewPodController(cli, &PodOptions{
+			Namespace: podControllerNS,
+			Name:      podControllerPodName,
+		}, WithPodControllerProcessor(pcp))
+
+		tc(pcp, pc)
+	}
+}
