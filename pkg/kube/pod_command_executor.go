c87872cd4632b860cd796853b397c96ebb98e3dc
Eugene Sumin <95425330+e-sumin@users.noreply.github.com>
2023-02-21 16:21:45 +0100

    Pod command executor implementation (#1903)
    * Implement PodController interface
    
    * Implement PodCommandExecutor interface
    
    * Update copyright year
    
    Co-authored-by: Pavan Navarathna <6504783+pavannd1@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Pavan Navarathna <6504783+pavannd1@users.noreply.github.com>

--- pkg/kube/pod_command_executor.go
+++ pkg/kube/pod_command_executor.go
@@ -17,9 +17,60 @@ package kube
 import (
 	"context"
 	"io"
+
+	"k8s.io/client-go/kubernetes"
 )
 
 // PodCommandExecutor allows us to execute command within the pod
 type PodCommandExecutor interface {
 	Exec(ctx context.Context, command []string, stdin io.Reader, stdout, stderr io.Writer) error
+}
+
+// podCommandExecutorProcessor aids in unit testing
+type podCommandExecutorProcessor interface {
+	execWithOptions(cli kubernetes.Interface, opts ExecOptions) (string, string, error)
+}
+
+// podCommandExecutor keeps everything required to execute command within a pod
+type podCommandExecutor struct {
+	cli           kubernetes.Interface
+	namespace     string
+	podName       string
+	containerName string
+
+	pcep podCommandExecutorProcessor
+}
+
+// Exec runs the command and logs stdout and stderr.
+func (p *podCommandExecutor) Exec(ctx context.Context, command []string, stdin io.Reader, stdout, stderr io.Writer) error {
+	var (
+		opts = ExecOptions{
+			Command:       command,
+			Namespace:     p.namespace,
+			PodName:       p.podName,
+			ContainerName: p.containerName,
+			Stdin:         stdin,
+			Stdout:        stdout,
+			Stderr:        stderr,
+		}
+		cmdDone = make(chan struct{})
+		err     error
+	)
+
+	go func() {
+		_, _, err = p.pcep.execWithOptions(p.cli, opts)
+		close(cmdDone)
+	}()
+
+	select {
+	case <-ctx.Done():
+		err = ctx.Err()
+	case <-cmdDone:
+	}
+
+	return err
+}
+
+func (p *podCommandExecutor) execWithOptions(cli kubernetes.Interface, opts ExecOptions) (string, string, error) {
+	return ExecWithOptions(p.cli, opts)
 }
