cf59a79050ea0b02f058d4162697b299d5e81a9a
Rajat Gupta <rajat.gupta@veeam.com>
2023-03-16 12:04:45 +0530

    Address Review
    Signed-off-by: Rajat Gupta <rajat.gupta@veeam.com>

--- pkg/datamover/datamover.go
+++ pkg/datamover/datamover.go
@@ -20,26 +20,24 @@ type DataMover interface {
 // the preferred DataMover as per the arguments passed in kando command
 func NewDataMover(c *cobra.Command) DataMover {
 	datamover := checkDataMover(c)
+	outputName := outputNameFlag(c)
+	snapJSON := kopiaSnapshotFlag(c)
 	switch datamover {
 	case profileFlagName:
-		outputName := outputNameFlag(c)
 		profileRef, err := unmarshalProfileFlag(c)
 		if err != nil {
 			return nil
 		}
-		snapJSON := kopiaSnapshotFlag(c)
 		return &Profile{
 			OutputName: outputName,
 			Profile:    profileRef,
 			SnapJSON:   snapJSON,
 		}
 	case repositoryServerFlagName:
-		outputName := outputNameFlag(c)
 		repositoryServerRef, err := unmarshalRepositoryServerFlag(c)
 		if err != nil {
 			return nil
 		}
-		snapJSON := kopiaSnapshotFlag(c)
 		return &RepositoryServer{
 			OutputName:       outputName,
 			RepositoryServer: repositoryServerRef,
