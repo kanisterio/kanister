845f8a6debcaca735b9837125e33c66a126d4f49
Eng Zer Jun <engzerjun@gmail.com>
2023-03-21 06:35:21 +0800

    Replace `github.com/ghodss/yaml` with `sigs.k8s.io/yaml` (#1964)
    At the time of making this commit, the package `github.com/ghodss/yaml`
    is no longer actively maintained.
    
    `sigs.k8s.io/yaml` is a permanent fork of `ghodss/yaml` and is actively
    maintained by Kubernetes SIG.
    
    Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>
    Co-authored-by: Julio Lopez <1953782+julio-lopez@users.noreply.github.com>

--- pkg/app/rds_postgres.go
+++ pkg/app/rds_postgres.go
@@ -24,12 +24,12 @@ import (
 	awssdk "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	awsrds "github.com/aws/aws-sdk-go/service/rds"
-	"github.com/ghodss/yaml"
 	"github.com/pkg/errors"
 	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/yaml"
 
 	crv1alpha1 "github.com/kanisterio/kanister/pkg/apis/cr/v1alpha1"
 	aws "github.com/kanisterio/kanister/pkg/aws"
