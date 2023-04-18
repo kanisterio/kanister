e1c0e9a03db626f06bdddeab763979f590e3f984
Pratik Shah <pratik@infracloud.io>
2023-04-18 11:44:32 +0530

    Add comment about empty import

--- pkg/apis/cr/v1alpha1/doc.go
+++ pkg/apis/cr/v1alpha1/doc.go
@@ -4,4 +4,7 @@
 // +groupName=cr
 package v1alpha1
 
+// While generating client files, we need code-generator package to be installed
+// but this package is not used anywhere hence go.mod removes this from
+// required package. hence added an empty import.
 import _ "k8s.io/code-generator"
