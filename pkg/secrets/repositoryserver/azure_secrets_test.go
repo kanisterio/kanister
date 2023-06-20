bda34386e2d2da071817a531f52d8675ff9fe304
Amruta Kale <amrutakale24.1991@gmail.com>
2023-06-20 18:38:55 +0530

    add unit tests for repository server location credentials

---
+++ pkg/secrets/repositoryserver/azure_secrets_test.go
@@ -0,0 +1,101 @@
+// Copyright 2023 The Kanister Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package repositoryserver
+
+import (
+	"github.com/pkg/errors"
+	. "gopkg.in/check.v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	secerrors "github.com/kanisterio/kanister/pkg/secrets/errors"
+)
+
+type AzureSecretCredsSuite struct{}
+
+var _ = Suite(&AzureSecretCredsSuite{})
+
+func (s *AzureSecretCredsSuite) TestValidateRepoServerAzureCredentials(c *C) {
+
+	for i, tc := range []struct {
+		secret        Secret
+		errChecker    Checker
+		expectedError error
+	}{
+		{ // Valid Azure Secret
+			secret: NewAzureLocation(&v1.Secret{
+				Type: v1.SecretType(LocTypeAzure),
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sec",
+					Namespace: "ns",
+				},
+				Data: map[string][]byte{
+					BucketKey: []byte("bucket"),
+					RegionKey: []byte("region"),
+				},
+			}),
+			errChecker: IsNil,
+		},
+		{ // Missing required field - Region Key
+			secret: NewAzureLocation(&v1.Secret{
+				Type: v1.SecretType(LocTypeAzure),
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sec",
+					Namespace: "ns",
+				},
+				Data: map[string][]byte{
+					BucketKey: []byte("bucket"),
+				},
+			}),
+			errChecker:    NotNil,
+			expectedError: errors.Wrapf(secerrors.ErrValidate, secerrors.MissingRequiredFieldErrorMsg, RegionKey, "ns", "sec"),
+		},
+		{ // Missing required field - Bucket Key
+			secret: NewAzureLocation(&v1.Secret{
+				Type: v1.SecretType(LocTypeAzure),
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sec",
+					Namespace: "ns",
+				},
+				Data: map[string][]byte{
+					BucketKey: []byte("region"),
+				},
+			}),
+			errChecker:    NotNil,
+			expectedError: errors.Wrapf(secerrors.ErrValidate, secerrors.MissingRequiredFieldErrorMsg, BucketKey, "ns", "sec"),
+		},
+		{ // Empty Secret
+			secret: NewAzureLocation(&v1.Secret{
+				Type: v1.SecretType(LocTypeAzure),
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sec",
+					Namespace: "ns",
+				},
+			}),
+			errChecker:    NotNil,
+			expectedError: errors.Wrapf(secerrors.ErrValidate, secerrors.EmptySecretErrorMessage, "ns", "sec"),
+		},
+		{ // Nil Secret
+			secret:        NewAzureLocation(nil),
+			errChecker:    NotNil,
+			expectedError: errors.Wrapf(secerrors.ErrValidate, secerrors.NilSecretErrorMessage, "ns", "sec"),
+		},
+	} {
+		err := tc.secret.Validate()
+		c.Check(err, tc.errChecker)
+		c.Check(err, Equals, tc.expectedError, Commentf("test number: %d", i))
+
+	}
+}
