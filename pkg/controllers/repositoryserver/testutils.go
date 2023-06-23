8d92463eec4b5ddb699a7d658b21a31019c02610
Amruta Kale <amrutakale24.1991@gmail.com>
2023-06-23 16:55:01 +0530

    move secret creation utils under test suite

--- pkg/controllers/repositoryserver/testutils.go
+++ pkg/controllers/repositoryserver/testutils.go
@@ -15,20 +15,7 @@
 package repositoryserver
 
 import (
-	"bytes"
-	"context"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
-	"math/big"
 	"os"
-	"time"
-
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 
 	crv1alpha1 "github.com/kanisterio/kanister/pkg/apis/cr/v1alpha1"
 	awsconfig "github.com/kanisterio/kanister/pkg/aws"
@@ -51,57 +38,6 @@ const (
 	pathKey                                    = "path"
 )
 
-func getKopiaTLSSecret() (map[string][]byte, error) {
-	ca := &x509.Certificate{
-		SerialNumber: big.NewInt(2019),
-		Subject: pkix.Name{
-			Organization:  []string{"Test Organization"},
-			Country:       []string{"Test Country"},
-			Province:      []string{"Test Province"},
-			Locality:      []string{"Test Locality"},
-			StreetAddress: []string{"Test Street"},
-			PostalCode:    []string{"123456"},
-		},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(0, 0, 1),
-		IsCA:                  true,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-		BasicConstraintsValid: true,
-	}
-	caPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return nil, err
-	}
-	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &caPrivKey.PublicKey, caPrivKey)
-	if err != nil {
-		return nil, err
-	}
-
-	caPEM := new(bytes.Buffer)
-	err = pem.Encode(caPEM, &pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: caBytes,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	caPrivKeyPEM := new(bytes.Buffer)
-	err = pem.Encode(caPrivKeyPEM, &pem.Block{
-		Type:  "PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(caPrivKey),
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return map[string][]byte{
-		"tls.crt": caPEM.Bytes(),
-		"tls.key": caPrivKeyPEM.Bytes(),
-	}, nil
-}
-
 func getDefaultS3StorageCreds() map[string][]byte {
 	key := os.Getenv(awsconfig.AccessKeyID)
 	val := os.Getenv(awsconfig.SecretAccessKey)
@@ -120,58 +56,4 @@ func getDefaultS3CompliantStorageLocation() map[string][]byte {
 		reposerver.RegionKey:   []byte(testutil.TestS3Region),
 		reposerver.EndpointKey: []byte(os.Getenv("LOCATION_ENDPOINT")),
 	}
-}
-
-func getRepoPasswordSecretData(password string) map[string][]byte {
-	return map[string][]byte{
-		reposerver.RepoPasswordKey: []byte(password),
-	}
-}
-
-func getRepoServerAdminSecretData(username, password string) map[string][]byte {
-	return map[string][]byte{
-		reposerver.AdminUsernameKey: []byte(username),
-		reposerver.AdminPasswordKey: []byte(password),
-	}
-}
-
-func getRepoServerUserAccessSecretData(hostname, password string) map[string][]byte {
-	return map[string][]byte{
-		hostname: []byte(password),
-	}
-}
-
-func createSecret(cli kubernetes.Interface, namespace, name string, secrettype v1.SecretType, data map[string][]byte) (se *v1.Secret, err error) {
-	secret := &v1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			GenerateName: name,
-		},
-		Data: data,
-	}
-	if secrettype != "" {
-		secret.Type = secrettype
-	}
-
-	se, err = cli.CoreV1().Secrets(namespace).Create(context.Background(), secret, metav1.CreateOptions{})
-	return
-}
-
-func createRepositoryServerAdminSecret(cli kubernetes.Interface, namespace string, data map[string][]byte) (se *v1.Secret, err error) {
-	return createSecret(cli, namespace, "test-repository-server-admin-", reposerver.AdminCredentialsSecret, data)
-}
-
-func createRepositoryServerUserAccessSecret(cli kubernetes.Interface, namespace string, data map[string][]byte) (se *v1.Secret, err error) {
-	return createSecret(cli, "test-repository-server-user-access-", namespace, "", data)
-}
-
-func createRepositoryPassword(cli kubernetes.Interface, namespace string, data map[string][]byte) (se *v1.Secret, err error) {
-	return createSecret(cli, "test-repository-password-", namespace, reposerver.RepositoryPasswordSecret, data)
-}
-
-func createKopiaTLSSecret(cli kubernetes.Interface, namespace string, data map[string][]byte) (se *v1.Secret, err error) {
-	return createSecret(cli, "test-repository-password-", namespace, v1.SecretTypeTLS, data)
-}
-
-func createStorageLocationSecret(cli kubernetes.Interface, namespace string, data map[string][]byte) (se *v1.Secret, err error) {
-	return createSecret(cli, "test-repository-server-storage-", namespace, reposerver.Location, data)
 }
