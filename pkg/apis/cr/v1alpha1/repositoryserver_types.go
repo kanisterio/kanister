9ec061edc4098e1f9fcb99fb6248c515f4778b41
Amruta Kale <amrutakale24.1991@gmail.com>
2023-06-28 12:19:59 +0530

    add fields for log,cache and config directory

--- pkg/apis/cr/v1alpha1/repositoryserver_types.go
+++ pkg/apis/cr/v1alpha1/repositoryserver_types.go
@@ -81,13 +81,18 @@ type Repository struct {
 	// PasswordSecretRef has the password required to connect to kopia repository
 	PasswordSecretRef corev1.SecretReference `json:"passwordSecretRef"`
 	CacheSizeSettings CacheSizeSettings      `json:"cacheSizeSettings,omitempty"`
+	// LogDirectory is an optional field to specify kopia log directory
+	LogDirectory string `json:"logDirectory,omitempty"`
+	// ConfigDirectory is an optional field to specify kopia config directory
+	ConfigFilePath string `json:"configFilePath,omitempty"`
 }
 
 // CacheSizeSettings are the metadata/content cache size details
 // that can be used while establishing connection to the kopia repository
 type CacheSizeSettings struct {
-	Metadata string `json:"metadata"`
-	Content  string `json:"content"`
+	Metadata       string `json:"metadata"`
+	Content        string `json:"content"`
+	CacheDirectory string `json:"cacheDirectory,omitempty"`
 }
 
 // Server details required for starting the repository proxy server and initializing the repository client users
