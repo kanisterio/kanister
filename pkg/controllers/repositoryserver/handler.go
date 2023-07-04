d9384c9ab9d53c0dafdbe158dc223c2d675a34ad
Akanksha Kumari <akankshakumari393@gmail.com>
2023-07-04 19:22:41 +0530

    Remove logs
    Signed-off-by: Akanksha Kumari <akankshakumari393@gmail.com>

--- pkg/controllers/repositoryserver/handler.go
+++ pkg/controllers/repositoryserver/handler.go
@@ -302,7 +302,6 @@ func (h *RepoServerHandler) waitForPodReady(ctx context.Context, pod *corev1.Pod
 }
 
 func (h *RepoServerHandler) updateProgressInCRStatus(ctx context.Context, progress crv1alpha1.RepositoryServerProgress) error {
-	h.Logger.Info("Fetch latest version of RepositoryServer to update the ServerInfo in its status")
 	repoServerName := h.RepositoryServer.Name
 	repoServerNamespace := h.RepositoryServer.Namespace
 	rs := crv1alpha1.RepositoryServer{}
@@ -310,13 +309,11 @@ func (h *RepoServerHandler) updateProgressInCRStatus(ctx context.Context, progre
 	if err != nil {
 		return err
 	}
-	h.Logger.Info("Update the Progress")
 	rs.Status.Progress = progress
 	err = h.Reconciler.Status().Update(ctx, &rs)
 	if err != nil {
 		return err
 	}
-	h.Logger.Info("Use this updated RepositoryServer CR")
 	h.RepositoryServer = &rs
 	return nil
 }
