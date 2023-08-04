c0bfa7529f1627c582bcee27fb5b4db53a8bc738
Amruta Kale <amruta.kale@veeam.com>
2023-08-04 15:41:26 +0530

    add omit empty for the fields
    Signed-off-by: Amruta Kale <amruta.kale@veeam.com>

--- pkg/apis/cr/v1alpha1/repositoryserver_types.go
+++ pkg/apis/cr/v1alpha1/repositoryserver_types.go
@@ -98,8 +98,8 @@ type Configuration struct {
 // CacheSizeSettings are the metadata/content cache size details
 // that can be used while establishing connection to the kopia repository
 type CacheSizeSettings struct {
-	Metadata *int `json:"metadata"`
-	Content  *int `json:"content"`
+	Metadata *int `json:"metadata,omitempty"`
+	Content  *int `json:"content,omitempty"`
 }
 
 // Server details required for starting the repository proxy server and initializing the repository client users
