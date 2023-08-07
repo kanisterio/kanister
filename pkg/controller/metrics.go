161b7897de63037b130a81ca68b4c54ec905a131
mellon-collie <nishantravishankar@gmail.com>
2023-08-07 17:32:20 -0400

    Added metrics file in controller

---
+++ pkg/controller/metrics.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	kanistermetrics "github.com/kanisterio/kanister/pkg/metrics"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type metrics struct {
+	actionSetResolutionCounterVec prometheus.CounterVec
+}
+
+const (
+	ACTION_SET_COUNTER_VEC_LABEL_RES         = "resolution"
+	ACTION_SET_COUNTER_VEC_LABEL_RES_SUCCESS = "success"
+	ACTION_SET_COUNTER_VEC_LABEL_RES_FAILURE = "failure"
+)
+
+func getActionSetCounterVecLabels() []kanistermetrics.BoundedLabel {
+	bl := make([]kanistermetrics.BoundedLabel, 1)
+	bl[0] = kanistermetrics.BoundedLabel{LabelName: ACTION_SET_COUNTER_VEC_LABEL_RES,
+		LabelValues: []string{ACTION_SET_COUNTER_VEC_LABEL_RES_SUCCESS, ACTION_SET_COUNTER_VEC_LABEL_RES_FAILURE}}
+	return bl
+}
+
+func newMetrics(reg prometheus.Registerer) *metrics {
+	actionSetCounterOpts := prometheus.CounterOpts{
+		Name: "action_set_resolutions_total",
+		Help: "Total number of action set resolutions",
+	}
+	actionSetResolutionCounterVec := kanistermetrics.InitCounterVec(reg, actionSetCounterOpts, getActionSetCounterVecLabels())
+	return &metrics{actionSetResolutionCounterVec: *actionSetResolutionCounterVec}
+}
