f968e1aebfc17c9d16aa22a5c5b0b90c932859c1
Pavan Navarathna Devaraj <6504783+pavannd1@users.noreply.github.com>
2023-08-10 22:56:15 -0700

    Add a helper to get kanister-tools-image from env

--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -1,3 +1,17 @@
+// Copyright 2023 The Kanister Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package consts
 
 const (
@@ -30,3 +44,6 @@ const RepositoryServerResourceNamePlural = "repositoryservers"
 
 const LatestKanisterToolsImage = "ghcr.io/kanisterio/kanister-tools:v9.99.9-dev"
 const KanisterToolsImage = "ghcr.io/kanisterio/kanister-tools:0.93.0"
+
+// KanisterToolsImageEnvName is used to set up a custom kanister-tools image
+const KanisterToolsImageEnvName = "KANISTER_TOOLS"
