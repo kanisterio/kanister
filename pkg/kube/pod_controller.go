65b2709ca1edfc93213bd6e46166520f0a72321d
Chaitali Gondhalekar <chaitali@kasten.io>
2023-08-15 09:00:50 -0700

    Fix issue with missing container name

--- pkg/kube/pod_controller.go
+++ pkg/kube/pod_controller.go
@@ -204,12 +204,24 @@ func (p *podController) StopPod(ctx context.Context, stopTimeout time.Duration, 
 	return nil
 }
 
+// getContainerName returns container name, which should be passed to operations which requires it
+// if container name was specified in podOptions, it will be used.
+// Otherwise, the first container name from specs will be taken as best effort (when pods are created with sidecars
+// sidecar containers are placed after main container).
+func (p *podController) getContainerName() string {
+	if p.podOptions.ContainerName != "" {
+		return p.podOptions.ContainerName
+	}
+
+	return p.pod.Spec.Containers[0].Name
+}
+
 func (p *podController) StreamPodLogs(ctx context.Context) (io.ReadCloser, error) {
 	if p.podName == "" {
 		return nil, ErrPodControllerPodNotStarted
 	}
 
-	return StreamPodLogs(ctx, p.cli, p.pod.Namespace, p.pod.Name, p.pod.Spec.Containers[0].Name)
+	return StreamPodLogs(ctx, p.cli, p.pod.Namespace, p.pod.Name, p.getContainerName())
 }
 
 func (p *podController) GetCommandExecutor() (PodCommandExecutor, error) {
