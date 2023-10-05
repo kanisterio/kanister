f273658ad923a7994087d7a3fb4145d9b38254c5
Sergey Aksenov <sergey.aksenov@veeam.com>
2023-10-05 14:09:50 +0200

    Error reporting fixed for checking status of `minio` (#2378)
    * error responding fixed for minio check
    
    * Comment in build/test.sh updated
    
    Co-authored-by: Vivek Singh <vsingh.ggits.2010@gmail.com>
    
    ---------
    
    Co-authored-by: Vivek Singh <vsingh.ggits.2010@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- build/test.sh
+++ build/test.sh
@@ -51,8 +51,8 @@ fi
 echo
 
 check_dependencies() {
-    # Check if minio is already deployed
-    if helm status minio -n minio > /dev/null 2>&1 ; then
+    # Check if minio is already deployed. We suppress only `stdout` and not `stderr` to make sure we catch errors if `helm status` fails
+    if helm status minio -n minio 1> /dev/null ; then
         # Setting env vars to access MinIO
         export S3_COMPLIANT_AWS_ACCESS_KEY_ID="AKIAIOSFODNN7EXAMPLE"
         export S3_COMPLIANT_AWS_SECRET_ACCESS_KEY="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
