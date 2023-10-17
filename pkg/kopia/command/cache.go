87d44346c9d41da8e6b965c67caad35c64becded
Amruta Kale <amruta.kale@veeam.com>
2023-10-17 17:24:06 +0530

    remove soft limit fields from the struct as we are not setting it
    Signed-off-by: Amruta Kale <amruta.kale@veeam.com>

--- pkg/kopia/command/cache.go
+++ pkg/kopia/command/cache.go
@@ -6,17 +6,19 @@ import (
 	"github.com/kanisterio/kanister/pkg/logsafe"
 )
 
+// CacheArgs has fields that can be used to set
+// cache settings for different kopia repository operations
 type CacheArgs struct {
-	ContentCacheMB       int
-	MetadataCacheMB      int
 	ContentCacheLimitMB  int
 	MetadataCacheLimitMB int
 }
 
 func (c CacheArgs) kopiaCacheArgs(args logsafe.Cmd, cacheDirectory string) logsafe.Cmd {
 	args = args.AppendLoggableKV(cacheDirectoryFlag, cacheDirectory)
-	args = args.AppendLoggableKV(contentCacheSizeMBFlag, strconv.Itoa(c.ContentCacheMB))
-	args = args.AppendLoggableKV(metadataCacheSizeMBFlag, strconv.Itoa(c.MetadataCacheMB))
+	// The hard limit flags for cache would be set using the env variables that are passed through
+	// helm settings
+	// Soft limit flags would be set to the default values by kopia automatically
+	// in connectOptions.setup() function as shown here - https://github.com/kopia/kopia/blob/master/cli/command_repository_connect.go#L71
 	args = args.AppendLoggableKV(contentCacheSizeLimitMBFlag, strconv.Itoa(c.ContentCacheLimitMB))
 	args = args.AppendLoggableKV(metadataCacheSizeLimitMBFlag, strconv.Itoa(c.MetadataCacheLimitMB))
 	return args
