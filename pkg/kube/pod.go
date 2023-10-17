b94a52b273711e02ca52be91f2fc26a3f7f804ee
Sergey Aksenov <sergey.aksenov@veeam.com>
2023-10-17 18:12:25 +0200

    Support of read only mount of filesystem volumes added (#2365)
    * Support of read only mount added
    
    Signed-off-by: Sergey Aksenov <sergey.aksenov@veeam.com>
    
    * review comments fixed
    
    Signed-off-by: Sergey Aksenov <sergey.aksenov@veeam.com>
    
    * renamings regarding review comments
    
    Signed-off-by: Sergey Aksenov <sergey.aksenov@veeam.com>
    
    ---------
    
    Signed-off-by: Sergey Aksenov <sergey.aksenov@veeam.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pkg/kube/pod.go
+++ pkg/kube/pod.go
@@ -48,6 +48,11 @@ const (
 	defaultContainerName   = "container"
 )
 
+type VolumeMountOptions struct {
+	MountPath string
+	ReadOnly  bool
+}
+
 // PodOptions specifies options for `CreatePod`
 type PodOptions struct {
 	Annotations        map[string]string
@@ -59,7 +64,7 @@ type PodOptions struct {
 	Labels             map[string]string
 	Namespace          string
 	ServiceAccountName string
-	Volumes            map[string]string
+	Volumes            map[string]VolumeMountOptions
 	BlockVolumes       map[string]string
 	// PodSecurityContext and ContainerSecurityContext can be used to set the security context
 	// at the pod level and container level respectively.
@@ -76,7 +81,7 @@ type PodOptions struct {
 	Lifecycle                *v1.Lifecycle
 }
 
-func GetPodObjectFromPodOptions(cli kubernetes.Interface, opts *PodOptions) (*v1.Pod, error) {
+func GetPodObjectFromPodOptions(ctx context.Context, cli kubernetes.Interface, opts *PodOptions) (*v1.Pod, error) {
 	// If Namespace is not specified, use the controller Namespace.
 	cns, err := GetControllerNamespace()
 	if err != nil {
@@ -101,7 +106,7 @@ func GetPodObjectFromPodOptions(cli kubernetes.Interface, opts *PodOptions) (*v1
 		opts.RestartPolicy = v1.RestartPolicyNever
 	}
 
-	volumeMounts, podVolumes, err := createFilesystemModeVolumeSpecs(opts.Volumes)
+	volumeMounts, podVolumes, err := createFilesystemModeVolumeSpecs(ctx, opts.Volumes)
 	if err != nil {
 		return nil, errors.Wrapf(err, "Failed to create volume spec")
 	}
@@ -207,10 +212,12 @@ func ContainerNameFromPodOptsOrDefault(po *PodOptions) string {
 
 // CreatePod creates a pod with a single container based on the specified image
 func CreatePod(ctx context.Context, cli kubernetes.Interface, opts *PodOptions) (*v1.Pod, error) {
-	pod, err := GetPodObjectFromPodOptions(cli, opts)
+	pod, err := GetPodObjectFromPodOptions(ctx, cli, opts)
 	if err != nil {
 		return nil, errors.Wrapf(err, "Failed to get pod from podOptions. Namespace: %s, NameFmt: %s", opts.Namespace, opts.GenerateName)
 	}
+
+	log.Debug().WithContext(ctx).Print("Creating POD", field.M{"name": pod.Name, "namespace": pod.Namespace})
 
 	pod, err = cli.CoreV1().Pods(pod.Namespace).Create(ctx, pod, metav1.CreateOptions{})
 	if err != nil {
