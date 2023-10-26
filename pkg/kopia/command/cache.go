6cd9c832a2f1151d90fb7f6470af25bf532094e5
Amruta Kale <amruta.kale@veeam.com>
2023-10-26 15:56:37 +0530

    format comments
    Signed-off-by: Amruta Kale <amruta.kale@veeam.com>

--- pkg/kopia/command/cache.go
+++ pkg/kopia/command/cache.go
@@ -29,10 +29,12 @@ type CacheArgs struct {
 
 func (c CacheArgs) kopiaCacheArgs(args logsafe.Cmd, cacheDirectory string) logsafe.Cmd {
 	args = args.AppendLoggableKV(cacheDirectoryFlag, cacheDirectory)
-	// The hard limit flags for cache would be set using the env variables that are passed through
-	// helm settings
-	// Soft limit flags would be set to the default values by kopia automatically
-	// in connectOptions.setup() function as shown here - https://github.com/kopia/kopia/blob/master/cli/command_repository_connect.go#L71
+
+	// The hard limit flags for cache would be set using the env variables that
+	// are passed through Kanister helm settings.
+	// Soft limit flags would be set to the default values by kopia
+	// automatically in ``connectOptions.setup()`` function.
+	// Refer to https://github.com/kopia/kopia/blob/master/cli/command_repository_connect.go#L71
 	args = args.AppendLoggableKV(contentCacheSizeLimitMBFlag, strconv.Itoa(c.ContentCacheLimitMB))
 	args = args.AppendLoggableKV(metadataCacheSizeLimitMBFlag, strconv.Itoa(c.MetadataCacheLimitMB))
 	return args
