05ebcf95d03a297a0d78eb599aa1f06e7092cc64
InfraQ <63741182+infraq@users.noreply.github.com>
2024-01-10 17:02:56 -0800

    Kanister docs update to version 0.104.0 (#2572)
    Co-authored-by: Kasten Production <infra@kasten.io>
    Co-authored-by: Pavan Navarathna <6504783+pavannd1@users.noreply.github.com>

--- pkg/function/kube_exec_test.go
+++ pkg/function/kube_exec_test.go
@@ -179,11 +179,11 @@ func (s *KubeExecTest) TestParseLogAndCreateOutput(c *C) {
 		errChecker Checker
 		outChecker Checker
 	}{
-		{"###Phase-output###: {\"key\":\"version\",\"value\":\"0.103.0\"}", map[string]interface{}{"version": "0.103.0"}, IsNil, NotNil},
-		{"###Phase-output###: {\"key\":\"version\",\"value\":\"0.103.0\"}\n###Phase-output###: {\"key\":\"path\",\"value\":\"/backup/path\"}",
-			map[string]interface{}{"version": "0.103.0", "path": "/backup/path"}, IsNil, NotNil},
-		{"Random message ###Phase-output###: {\"key\":\"version\",\"value\":\"0.103.0\"}", map[string]interface{}{"version": "0.103.0"}, IsNil, NotNil},
-		{"Random message with newline \n###Phase-output###: {\"key\":\"version\",\"value\":\"0.103.0\"}", map[string]interface{}{"version": "0.103.0"}, IsNil, NotNil},
+		{"###Phase-output###: {\"key\":\"version\",\"value\":\"0.104.0\"}", map[string]interface{}{"version": "0.104.0"}, IsNil, NotNil},
+		{"###Phase-output###: {\"key\":\"version\",\"value\":\"0.104.0\"}\n###Phase-output###: {\"key\":\"path\",\"value\":\"/backup/path\"}",
+			map[string]interface{}{"version": "0.104.0", "path": "/backup/path"}, IsNil, NotNil},
+		{"Random message ###Phase-output###: {\"key\":\"version\",\"value\":\"0.104.0\"}", map[string]interface{}{"version": "0.104.0"}, IsNil, NotNil},
+		{"Random message with newline \n###Phase-output###: {\"key\":\"version\",\"value\":\"0.104.0\"}", map[string]interface{}{"version": "0.104.0"}, IsNil, NotNil},
 		{"###Phase-output###: Invalid message", nil, NotNil, IsNil},
 		{"Random message", nil, IsNil, IsNil},
 	} {
