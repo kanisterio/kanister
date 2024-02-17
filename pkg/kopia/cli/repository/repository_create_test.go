74a6567d75d5aaaf634d05324aa183fa07dec0d7
pavel.larkin <pavel.larkin@veeam.com>
2024-02-17 15:43:46 -0800

    Reorganize tests
    Signed-off-by: pavel.larkin <pavel.larkin@veeam.com>

--- pkg/kopia/cli/repository/repository_create_test.go
+++ pkg/kopia/cli/repository/repository_create_test.go
@@ -16,71 +16,15 @@ package repository
 
 import (
 	"testing"
-	"time"
 
 	"github.com/kanisterio/safecli"
 	"gopkg.in/check.v1"
 
 	"github.com/kanisterio/kanister/pkg/kopia/cli"
-	"github.com/kanisterio/kanister/pkg/kopia/cli/args"
-	"github.com/kanisterio/kanister/pkg/kopia/cli/internal"
 	"github.com/kanisterio/kanister/pkg/kopia/cli/internal/test"
-	rs "github.com/kanisterio/kanister/pkg/secrets/repositoryserver"
 )
 
-func TestRepositoryCommands(t *testing.T) { check.TestingT(t) }
-
-var (
-	common = args.Common{
-		RepoPassword:   "encr-key",
-		ConfigFilePath: "path/kopia.config",
-		LogDirectory:   "cache/log",
-	}
-
-	cache = args.Cache{
-		CacheDirectory:           "/tmp/cache.dir",
-		ContentCacheSizeLimitMB:  0,
-		MetadataCacheSizeLimitMB: 0,
-	}
-
-	retentionMode   = "Locked"
-	retentionPeriod = 15 * time.Minute
-
-	locFS = internal.Location{
-		rs.TypeKey:   []byte("filestore"),
-		rs.PrefixKey: []byte("test-prefix"),
-	}
-
-	locAzure = internal.Location{
-		rs.TypeKey:   []byte("azure"),
-		rs.BucketKey: []byte("test-bucket"),
-		rs.PrefixKey: []byte("test-prefix"),
-	}
-
-	locGCS = internal.Location{
-		rs.TypeKey:   []byte("gcs"),
-		rs.BucketKey: []byte("test-bucket"),
-		rs.PrefixKey: []byte("test-prefix"),
-	}
-
-	locS3 = internal.Location{
-		rs.TypeKey:          []byte("s3"),
-		rs.EndpointKey:      []byte("test-endpoint"),
-		rs.RegionKey:        []byte("test-region"),
-		rs.BucketKey:        []byte("test-bucket"),
-		rs.PrefixKey:        []byte("test-prefix"),
-		rs.SkipSSLVerifyKey: []byte("false"),
-	}
-
-	locS3Compliant = internal.Location{
-		rs.TypeKey:          []byte("s3Compliant"),
-		rs.EndpointKey:      []byte("test-endpoint"),
-		rs.RegionKey:        []byte("test-region"),
-		rs.BucketKey:        []byte("test-bucket"),
-		rs.PrefixKey:        []byte("test-prefix"),
-		rs.SkipSSLVerifyKey: []byte("false"),
-	}
-)
+func TestRepositoryCreateCommand(t *testing.T) { check.TestingT(t) }
 
 // Test Repository Create command
 var _ = check.Suite(test.NewCommandSuite([]test.CommandTest{
