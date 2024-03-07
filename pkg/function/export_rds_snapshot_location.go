b96ba492bd35810552f3a0edddab4181f7172eb9
InfraQ <63741182+infraq@users.noreply.github.com>
2024-03-06 16:36:17 -0800

    Kanister docs update to version 0.106.0 (#2726)
    Co-authored-by: Kasten Production <infra@kasten.io>

--- pkg/function/export_rds_snapshot_location.go
+++ pkg/function/export_rds_snapshot_location.go
@@ -64,7 +64,7 @@ const (
 	BackupAction  RDSAction = "backup"
 	RestoreAction RDSAction = "restore"
 
-	postgresToolsImage = "ghcr.io/kanisterio/postgres-kanister-tools:0.105.0"
+	postgresToolsImage = "ghcr.io/kanisterio/postgres-kanister-tools:0.106.0"
 )
 
 type exportRDSSnapshotToLocationFunc struct {
