cc295a8df062fac5a4a3176bde3a716ed9a31479
Abhijit Mukherjee <abhijit.mukherjee@infracloud.io>
2024-04-02 10:42:17 +0530

    Added unit test for jobid debug label in ephemeral pod
    Signed-off-by: Abhijit Mukherjee <abhijit.mukherjee@infracloud.io>

--- pkg/kube/pod_runner_test.go
+++ pkg/kube/pod_runner_test.go
@@ -18,11 +18,15 @@ import (
 	"context"
 	"os"
 
+	"github.com/pkg/errors"
 	. "gopkg.in/check.v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/testing"
+
+	"github.com/kanisterio/kanister/pkg/consts"
+	"github.com/kanisterio/kanister/pkg/field"
 )
 
 type PodRunnerTestSuite struct{}
@@ -109,9 +113,65 @@ func (s *PodRunnerTestSuite) TestPodRunnerForSuccessCase(c *C) {
 	cancel()
 }
 
+// TestPodRunnerWithJobIDDebugLabelForSuccessCase: This test adds a debug entry (kanister.io/JobID) into the context and verifies the
+// pod got created with corresponding label using the entry or not.
+func (s *PodRunnerTestSuite) TestPodRunnerWithJobIDDebugLabelForSuccessCase(c *C) {
+	ctx, cancel := context.WithCancel(context.Background())
+	randomUUID := "xyz123"
+	ctx = field.Context(ctx, consts.LabelPrefix+"JobID", randomUUID)
+
+	cli := fake.NewSimpleClientset()
+	cli.PrependReactor("create", "pods", func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+		return false, nil, nil
+	})
+	cli.PrependReactor("get", "pods", func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+		p := &corev1.Pod{
+			Status: corev1.PodStatus{
+				Phase: corev1.PodRunning,
+			},
+		}
+		return true, p, nil
+	})
+	deleted := make(chan struct{})
+	cli.PrependReactor("delete", "pods", func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+		c.Log("Pod deleted due to Context Cancelled")
+		close(deleted)
+		return true, nil, nil
+	})
+	po := &PodOptions{
+		Namespace: podRunnerNS,
+		Name:      podName,
+		Command:   []string{"sh", "-c", "tail -f /dev/null"},
+	}
+	AddLabelsToPodOptionsFromContext(ctx, po, consts.LabelPrefix, "JobID")
+	pr := NewPodRunner(cli, po)
+	errorCh := make(chan error)
+	go func() {
+		_, err := pr.Run(ctx, makePodRunnerTestForKeyPresence(consts.LabelPrefix+"JobID", randomUUID, deleted))
+		errorCh <- err
+	}()
+	deleted <- struct{}{}
+	c.Assert(<-errorCh, IsNil)
+	cancel()
+}
+
 func makePodRunnerTestFunc(ch chan struct{}) func(ctx context.Context, pc PodController) (map[string]interface{}, error) {
 	return func(ctx context.Context, pc PodController) (map[string]interface{}, error) {
 		<-ch
+		return nil, nil
+	}
+}
+
+func makePodRunnerTestForKeyPresence(labelKey, labelValue string, ch chan struct{}) func(ctx context.Context, pc PodController) (map[string]interface{}, error) {
+	return func(ctx context.Context, pc PodController) (map[string]interface{}, error) {
+		<-ch
+		value, ok := pc.Pod().Labels[labelKey]
+		if !ok {
+			return nil, errors.New("Key not present")
+		}
+		if value != labelValue {
+			return nil, errors.New("Value mismatch")
+		}
 		return nil, nil
 	}
 }
