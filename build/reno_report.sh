440eeb86ca141e17c7cd8b98079f2e9cd0959f48
Daniil Fedotov <daniil.fedotov@kasten.io>
2024-04-09 16:53:46 -0400

    Document reno_report script

--- build/reno_report.sh
+++ build/reno_report.sh
@@ -2,11 +2,24 @@
 
 version=${1:-"''"}
 
+## Generate changelog files using reno
+
+## Used tools:
+## - reno for changelog generation
+## - rst2md for report conversion from rst to markdown
+
+## This script generates changelog files in markdown format.
+## Generated files are:
+## - CHANGELOG.md contains all versions history (for versions which have reno notes) 
+## - CHANGELOG_CURRENT.md contains only current version changes
+
+## Arguments:
+## version ($1) - string to use for "current" version number  
+
 ## Latest notes should go to current version
 ## We generate report before tagging the repo, so we need to set this version here
 # echo "unreleased_version_title: ${version}" > reno.yaml
 sed "s/unreleased_version_title: ''/unreleased_version_title: ${version}/g" reno.yaml > releasenotes/config.yaml
-
 
 # Update changelog for all versions:
 
