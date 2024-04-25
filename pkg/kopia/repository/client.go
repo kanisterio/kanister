766625f181310d0a7008d3cf84ecb9d066c978f5
Nick Wright <nicholas.wright@veeam.com>
2024-04-25 00:13:41 +0000

    Remove nogrpc option missed in previous PR

--- pkg/kopia/repository/client.go
+++ pkg/kopia/repository/client.go
@@ -56,8 +56,6 @@ func ConnectToAPIServer(
 	serverInfo := &repo.APIServerInfo{
 		BaseURL:                             serverAddress,
 		TrustedServerCertificateFingerprint: fingerprint,
-		// TODO(@pavan): Remove once GRPC support is added (kopia 0.8 release)
-		DisableGRPC: true,
 	}
 
 	opts := &repo.ConnectOptions{
