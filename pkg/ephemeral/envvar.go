9e8553928dc10cc2fd1db981112929d735f408ef
Eliott Gilson <eliott.gilson@veeam.com>
2024-05-10 14:09:16 -0700

    After Pavan's review

--- pkg/ephemeral/envvar.go
+++ pkg/ephemeral/envvar.go
@@ -1,13 +1,28 @@
+// Copyright 2024 The Kanister Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ephemeral
 
 import (
 	"os"
 
-	"github.com/kanisterio/kanister/pkg/kube"
 	corev1 "k8s.io/api/core/v1"
+
+	"github.com/kanisterio/kanister/pkg/kube"
 )
 
-// OSEnvVar creates an ApplierSet to set an environment variable if its present
+// OSEnvVar creates an ApplierSet to set an environment variable if it's present
 // in the current environment.
 func OSEnvVar(name string) ApplierSet {
 	return ApplierSet{
--- pkg/ephemeral/ephemeral.go
+++ pkg/ephemeral/ephemeral.go
@@ -1,8 +1,23 @@
+// Copyright 2024 The Kanister Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ephemeral
 
 import (
-	"github.com/kanisterio/kanister/pkg/kube"
 	corev1 "k8s.io/api/core/v1"
+
+	"github.com/kanisterio/kanister/pkg/kube"
 )
 
 var (
@@ -10,7 +25,7 @@ var (
 	PodOptions ApplierList[kube.PodOptions]
 )
 
-// ApplierSet is a group of Appliers, typically returned by an constructor.
+// ApplierSet is a group of Appliers, typically returned by a constructor.
 type ApplierSet struct {
 	Container  Applier[corev1.Container]
 	PodOptions Applier[kube.PodOptions]
@@ -87,7 +102,7 @@ func (f FiltererFunc[T]) Filter(options *T) bool {
 	return f(options)
 }
 
-// Filter applies the Applier's if the Filterer criterion is met.
+// Filter applies the Appliers if the Filterer criterion is met.
 func Filter[T Constraint](filterer Filterer[T], appliers ...Applier[T]) Applier[T] {
 	return ApplierFunc[T](func(options *T) {
 		if !filterer.Filter(options) {
@@ -100,7 +115,8 @@ func Filter[T Constraint](filterer Filterer[T], appliers ...Applier[T]) Applier[
 	})
 }
 
-// PodOptionsNameFilter is a Filterer that filters based on the PodOptions.Name.
+// PodOptionsNameFilter is a Filterer that filters based on the PodOptions.Name
+// which is the Pod name.
 type PodOptionsNameFilter string
 
 func (n PodOptionsNameFilter) Filter(options *kube.PodOptions) bool {
