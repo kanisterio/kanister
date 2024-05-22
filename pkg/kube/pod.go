7383bae8404ae3a685e455ba6aa548a62aed4aab
Wojciech Chojnowski <w.chojnowski@piwik.pro>
2024-05-22 16:10:00 +0200

    DEVOPS-7803 restore log stream
    Re-establish connection while the pod is still running
    
    Signed-off-by: Daniil Fedotov <daniil.fedotov@kasten.io>

--- pkg/kube/pod.go
+++ pkg/kube/pod.go
@@ -330,10 +330,13 @@ func DeletePod(ctx context.Context, cli kubernetes.Interface, pod *corev1.Pod) e
 	return nil
 }
 
-func StreamPodLogs(ctx context.Context, cli kubernetes.Interface, namespace, podName, containerName string) (io.ReadCloser, error) {
+func StreamPodLogs(ctx context.Context, cli kubernetes.Interface, namespace, podName, containerName string, sinceTime *metav1.Time) (io.ReadCloser, error) {
 	plo := &corev1.PodLogOptions{
 		Follow:    true,
 		Container: containerName,
+	}
+	if sinceTime != nil {
+		plo.SinceTime = sinceTime
 	}
 	return cli.CoreV1().Pods(namespace).GetLogs(podName, plo).Stream(ctx)
 }
@@ -354,7 +357,7 @@ func GetPodLogs(ctx context.Context, cli kubernetes.Interface, namespace, podNam
 
 // getErrorFromLogs fetches logs from pod and constructs error containing last ten lines of log and specified error message
 func getErrorFromLogs(ctx context.Context, cli kubernetes.Interface, namespace, podName, containerName string, err error, errorMessage string) error {
-	r, logErr := StreamPodLogs(ctx, cli, namespace, podName, containerName)
+	r, logErr := StreamPodLogs(ctx, cli, namespace, podName, containerName, nil)
 	if logErr != nil {
 		return errkit.Wrap(logErr, "Failed to fetch logs from the pod")
 	}
