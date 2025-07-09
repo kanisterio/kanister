1d465e66b959893bf8b23cbea7adc1db4aed73c0
Eliott Gilson <eliott.gilson@veeam.com>
2025-07-08 17:04:30 -0700

    Added annotations ephemeral applier set

---
+++ pkg/ephemeral/annotations.go
@@ -0,0 +1,47 @@
+package ephemeral
+
+import (
+	"encoding/json"
+	"maps"
+	"os"
+
+	"github.com/kanisterio/kanister/pkg/kube"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func AnnotationsFromEnvVar(name string) ApplierSet {
+	return ApplierSet{
+		PodOptions: ApplierFunc[kube.PodOptions](func(options *kube.PodOptions) error {
+			if val, present := os.LookupEnv(name); present {
+				var annotations map[string]string
+				if err := json.Unmarshal([]byte(val), annotations); err != nil {
+					return err
+				}
+
+				if options.Annotations == nil {
+					options.Annotations = make(map[string]string)
+				}
+
+				maps.Insert(options.Annotations, maps.All(annotations))
+			}
+
+			return nil
+		}),
+		Pod: ApplierFunc[corev1.Pod](func(options *corev1.Pod) error {
+			if val, present := os.LookupEnv(name); present {
+				var annotations map[string]string
+				if err := json.Unmarshal([]byte(val), annotations); err != nil {
+					return err
+				}
+
+				if options.Annotations == nil {
+					options.Annotations = make(map[string]string)
+				}
+
+				maps.Insert(options.Annotations, maps.All(annotations))
+			}
+
+			return nil
+		}),
+	}
+}
