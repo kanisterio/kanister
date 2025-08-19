1328571e3f54f444923755a314615220db76dc49
Varuni Agarwal <v.agarwal@veeam.com>
2025-08-19 10:21:12 +0530

    removed deprecated volume functions (#3581)
    Signed-off-by: varuni-dev <v.agarwal@veeam.com>

--- pkg/utils/volumesnapshot/helper_test.go
+++ pkg/utils/volumesnapshot/helper_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package blockstorage
+package volumesnapshot
 
 import (
 	"gopkg.in/check.v1"
--- pkg/utils/volumesnapshot/tags_test.go
+++ pkg/utils/volumesnapshot/tags_test.go
@@ -1,4 +1,4 @@
-package tags
+package volumesnapshot
 
 import (
 	"testing"
