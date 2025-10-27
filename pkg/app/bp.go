2fd7a63886cae33ee3410918e560f2448c5219f2
varuni-dev <v.agarwal@veeam.com>
2025-10-27 15:45:45 +0530

    support to read blueprint from new bp repo
    Signed-off-by: varuni-dev <v.agarwal@veeam.com>

--- pkg/app/bp.go
+++ pkg/app/bp.go
@@ -19,52 +19,63 @@ import (
 	"strings"
 	"time"
 
+	"github.com/kanisterio/blueprints"
 	"k8s.io/apimachinery/pkg/util/rand"
 
 	crv1alpha1 "github.com/kanisterio/kanister/pkg/apis/cr/v1alpha1"
-	bp "github.com/kanisterio/kanister/pkg/blueprint"
 	"github.com/kanisterio/kanister/pkg/field"
 	"github.com/kanisterio/kanister/pkg/log"
 )
 
 const (
-	blueprintsRepo = "./blueprints"
 	// imagePrefix specifies the prefix an image is going to have if it's being consumed from
 	// kanister's ghcr registry
 	imagePrefix = "ghcr.io/kanisterio"
 )
 
 // AppBlueprint implements Blueprint() to return Blueprint specs for the app
-// Blueprint() returns Blueprint placed at ./blueprints/{app-name}-blueprint.yaml
+// Blueprint() returns the Blueprint located at {app-name}/{app-name}-blueprint.yaml in blueprint repository
 type AppBlueprint struct {
-	App          string
-	Path         string
-	UseDevImages bool
+	App           string
+	Path          string
+	UseDevImages  bool
+	isKanisterApp bool
 }
 
 // PITRBlueprint implements Blueprint() to return Blueprint with PITR
-// Blueprint() returns Blueprint placed at ./blueprints/{app-name}-blueprint.yaml
+// Blueprint() returns Blueprint located at {app-name}/{app-name}-blueprint.yaml in blueprint repository
 type PITRBlueprint struct {
 	AppBlueprint
 }
 
-func NewBlueprint(app string, bpReposPath string, useDevImages bool) Blueprinter {
-	if bpReposPath == "" {
-		bpReposPath = blueprintsRepo
+func NewBlueprint(app string, blueprintName string, blueprintPath string, useDevImages bool) Blueprinter {
+	isKanisterApp := false
+
+	if blueprintPath == "" {
+		blueprintPath = getBlueprintPath(app, blueprintName)
+		isKanisterApp = true
 	}
 	return &AppBlueprint{
-		App:          app,
-		Path:         fmt.Sprintf("%s/%s-blueprint.yaml", bpReposPath, app),
-		UseDevImages: useDevImages,
+		App:           app,
+		Path:          blueprintPath,
+		UseDevImages:  useDevImages,
+		isKanisterApp: isKanisterApp,
 	}
 }
 
 func (b AppBlueprint) Blueprint() *crv1alpha1.Blueprint {
-	bpr, err := bp.ReadFromFile(b.Path)
+	var bpr *crv1alpha1.Blueprint
+	var err error
+
+	if b.isKanisterApp {
+		bpr, err = blueprints.ReadFromEmbeddedFile(b.Path)
+	} else {
+		bpr, err = blueprints.ReadFromFile(b.Path)
+	}
+
 	if err != nil {
 		log.Error().WithError(err).Print("Failed to read Blueprint", field.M{"app": b.App})
 	}
-
 	// set the name to a dynamically generated value
 	// so that the name wont conflict with the same application
 	// installed using other ways
@@ -109,15 +120,12 @@ func updateImageTags(bp *crv1alpha1.Blueprint) {
 	}
 }
 
-// NewPITRBlueprint returns blueprint placed at ./blueprints/{app-name}-blueprint.yaml
-func NewPITRBlueprint(app string, bpReposPath string, useDevImages bool) Blueprinter {
-	if bpReposPath == "" {
-		bpReposPath = blueprintsRepo
-	}
+// NewPITRBlueprint returns blueprint located at {app-name}/{app-name}-blueprint.yaml in blueprint repository
+func NewPITRBlueprint(app string, blueprintName string, useDevImages bool) Blueprinter {
 	return &PITRBlueprint{
 		AppBlueprint{
 			App:          app,
-			Path:         fmt.Sprintf("%s/%s-blueprint.yaml", bpReposPath, app),
+			Path:         getBlueprintPath(app, blueprintName),
 			UseDevImages: useDevImages,
 		},
 	}
@@ -125,4 +133,26 @@ func NewPITRBlueprint(app string, bpReposPath string, useDevImages bool) Bluepri
 
 func (b PITRBlueprint) FormatPITR(pitr time.Time) string {
 	return pitr.UTC().Format("2006-01-02T15:04:05Z")
+}
+
+func getBlueprintPath(app string, blueprintName string) string {
+	var blueprintFolder string
+
+	// If blueprintName is not provided, use app name as blueprint name
+	if blueprintName == "" {
+		blueprintName = app
+	}
+
+	switch app {
+	case "rds-aurora-snap":
+		blueprintFolder = "aws-rds-aurora-mysql"
+	case "rds-postgres-snap", "rds-postgres", "rds-postgres-dump":
+		blueprintFolder = "aws-rds-postgres"
+	case "kafka":
+		blueprintFolder = "kafka-adobe-s3-connector"
+	default:
+		blueprintFolder = blueprintName
+	}
+
+	return fmt.Sprintf("%s/%s-blueprint.yaml", blueprintFolder, blueprintName)
 }
