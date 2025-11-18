b455aec39eb71d73dec0b2eaa2e9e13365e2c138
Timothy Mamo <tim@themamo.com>
2025-11-18 16:35:26 +0100

    Looping through input array

--- build/bump_version.sh
+++ build/bump_version.sh
@@ -20,31 +20,33 @@ set -o xtrace
 
 
 usage() {
-    echo ./build/bump_version.sh previous_versions[2] release_version
+    echo ./build/bump_version.sh array_length previous_versions[2] release_version
     exit 1
 }
 
 main() {
-    local prev=( $1 $2 ); shift 2
+    local array_size=${1:?"$(usage)"}; shift
+    local prev=${1:?"$(usage)"}; shift
+    for (( j=1; j<${array_size}; j++ )); do
+        prev+=( $1 ); shift
+    done
+    echo "${prev[@]}"
+
     local next=${1:?"$(usage)"}; shift
-    echo $prev
-    echo $next
     if [ "$#" -eq 0 ]; then
             pkgs=( docker/ scripts/ examples/ pkg/ helm/ )
         else
             pkgs=( "$@" )
     fi
-    echo $pkgs
-  
-    
+
     for ((i=${#prev[@]}-1; i>=0; i--)); do
+        echo "Processing version: ${prev[$i]}"
         # -F matches for exact words, not regular expression (-E), that is what required here
         grep -F "${prev[$i]}" -Ir  "${pkgs[@]}" --exclude-dir={docs,mod,bin,html,frontend} --exclude=\*.sum --exclude=\*.mod | cut -d ':' -f 1 | uniq | xargs sed -ri "s/${prev[$i]}/${next//./\\.}/g"
     done
 
     # Modify the first instabnce of kanister_tools_version in docs/constants.ts
-    array_size=${#pkgs[@]}
-    if [ $array_size -eq 1 ]; then
+    if [ ${#pkgs[@]} -eq 1 ]; then
       file_count=$(find "${pkgs[@]}/docs" -maxdepth 1 -name "constants.ts" | wc -l)
       if [ $file_count -eq 1 ]; then
           sed -i "0,/'kanister_tools_version': *,/s/'kanister_tools_version': '${next//./\\.}',/" "${pkgs[@]}/docs/constants.ts"
