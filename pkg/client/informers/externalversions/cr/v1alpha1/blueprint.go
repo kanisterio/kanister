02ea9a7af968f067d097bf844483b333f41ab985
varuni-dev <v.agarwal@veeam.com>
2026-07-22 15:46:32 +0530

    deps(go): bump k8s.io deps to v0.35.1 and regenerate client
    Signed-off-by: varuni-dev <v.agarwal@veeam.com>

--- pkg/client/informers/externalversions/cr/v1alpha1/blueprint.go
+++ pkg/client/informers/externalversions/cr/v1alpha1/blueprint.go
@@ -57,7 +57,7 @@ func NewBlueprintInformer(client versioned.Interface, namespace string, resyncPe
 // one. This reduces memory footprint and number of connections to the server.
 func NewFilteredBlueprintInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
 	return cache.NewSharedIndexInformer(
-		&cache.ListWatch{
+		cache.ToListWatcherWithWatchListSemantics(&cache.ListWatch{
 			ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
@@ -82,7 +82,7 @@ func NewFilteredBlueprintInformer(client versioned.Interface, namespace string, 
 				}
 				return client.CrV1alpha1().Blueprints(namespace).Watch(ctx, options)
 			},
-		},
+		}, client),
 		&apiscrv1alpha1.Blueprint{},
 		resyncPeriod,
 		indexers,
